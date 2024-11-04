64eeb9ec7bd5bdbcd6ddc072636058e3a14b478c
Minhyung Cho <dup33333@hufs.ac.kr>
2024-11-04 20:00:52 +0900

    feat : 헤더 컴포넌트 퍼블리싱

---
+++ client/src/pages/Main/index.tsx
@@ -0,0 +1,9 @@
+import Header from "@/components/Header/Header";
+
+export default function MainPage() {
+  return (
+    <main className="w-screen h-screen flex flex-col">
+      <Header />
+    </main>
+  );
+}
