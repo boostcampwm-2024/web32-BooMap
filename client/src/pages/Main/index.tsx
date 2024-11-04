cc5645a6b781afa195226f42544d3d338f27aafd
Minhyung Cho <dup33333@hufs.ac.kr>
2024-11-05 01:08:55 +0900

    feat : MainPage 컴포넌트에 MainSection 컴포넌트 추가

--- client/src/pages/Main/index.tsx
+++ client/src/pages/Main/index.tsx
@@ -1,12 +1,14 @@
 import Header from "@/components/Header";
+import MainSection from "@/components/MainSection";
 import Sidebar from "@/components/Sidebar";
 
 export default function MainPage() {
   return (
     <main className="w-screen h-screen flex">
       <Sidebar />
-      <div className="w-full flex flex-col">
+      <div className="w-full h-screen flex flex-col">
         <Header />
+        <MainSection />
       </div>
     </main>
   );
