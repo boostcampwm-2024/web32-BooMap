5c981ee89fad75cec56d7f432495746a941bba11
Minhyung Cho <dup33333@hufs.ac.kr>
2024-11-05 21:55:27 +0900

    refactor : overviewButton 컴포넌트에 props 전달 및 활성화 항목 제어

--- client/src/components/Sidebar/Overview.tsx
+++ client/src/components/Sidebar/Overview.tsx
@@ -1,22 +1,50 @@
-import { Button } from "@headlessui/react";
 import dashBoardIcon from "@/assets/dashboard.png";
 import listIcon from "@/assets/list.png";
 import aiIcon from "@/assets/ai.png";
 import voiceIcon from "@/assets/voiceFile.png";
 import textIcon from "@/assets/textFile.png";
 import OverviewButton from "@/components/Sidebar/OverviewButton";
+import useSection from "@/hooks/useSection";
 
 export default function Overview() {
+  const { getmode, handleViewMode } = useSection();
   return (
-    <div className="flex flex-col mt-14 text-lg">
-      <p className="text-grayscale-500 pl-8 mb-8 font-medium">OVERVIEW</p>
-      <div className="flex flex-col gap-7 text-grayscale-400 px-8">
-        <OverviewButton src={dashBoardIcon} alt="대시보드" text="대시보드" />
-        <OverviewButton src={listIcon} alt="리스트" text="리스트로 보기" />
-        <OverviewButton src={aiIcon} alt="AI" text="AI 변환" />
-        <div className="flex flex-col gap-7 pl-10">
-          <OverviewButton src={voiceIcon} alt="음성 파일" text="음성 파일 업로드" />
-          <OverviewButton src={textIcon} alt="텍스트 파일" text="텍스트 파일 업로드" />
+    <div className="mt-14 flex flex-col text-lg">
+      <p className="mb-8 font-medium text-grayscale-500">OVERVIEW</p>
+      <div className="flex flex-col gap-2 text-grayscale-400">
+        <OverviewButton
+          src={dashBoardIcon}
+          alt="대시보드"
+          text="대시보드"
+          active={getmode() === "dashboard"}
+          onclick={() => handleViewMode("dashboard")}
+        />
+        <OverviewButton
+          src={listIcon}
+          alt="리스트"
+          text="리스트로 보기"
+          active={getmode() === "listview"}
+          onclick={() => handleViewMode("listview")}
+        />
+        <div className="flex items-center gap-6 rounded-lg p-3">
+          <img src={aiIcon} alt="ai변환" className="w-6" />
+          AI 변환
+        </div>
+        <div className="flex flex-col gap-2 pl-5">
+          <OverviewButton
+            src={voiceIcon}
+            alt="음성 파일"
+            text="음성 파일 업로드"
+            active={getmode() === "voiceupload"}
+            onclick={() => handleViewMode("voiceupload")}
+          />
+          <OverviewButton
+            src={textIcon}
+            alt="텍스트 파일"
+            text="텍스트 파일 업로드"
+            active={getmode() === "textupload"}
+            onclick={() => handleViewMode("textupload")}
+          />
         </div>
       </div>
     </div>
