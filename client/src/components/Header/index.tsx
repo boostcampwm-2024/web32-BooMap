285abbddda531238bf44fa830fde7758ff50789e
Minhyung Cho <dup33333@hufs.ac.kr>
2024-11-05 23:25:34 +0900

    feat : 로그인 모달 퍼블리싱

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -2,24 +2,30 @@ import { Button } from "@headlessui/react";
 import profile from "@/assets/profile.png";
 import shareIcon from "@/assets/share.png";
 import exportIcon from "@/assets/export.png";
+import useModal from "@/hooks/useModal";
+import LoginModal from "@/components/LoginModal";
 
 export default function Header() {
+  const { open, openModal, closeModal } = useModal();
   return (
-    <header className="w-full bg-grayscale-700 flex p-4 justify-between">
+    <header className="flex w-full justify-between bg-grayscale-700 p-4">
       <div className="flex gap-5">
-        <Button className="bg-bm-blue text-grayscale-100 px-5 rounded-lg flex items-center gap-3">
+        <Button className="flex items-center gap-3 rounded-lg bg-bm-blue px-5 text-grayscale-100">
           <img src={shareIcon} alt="공유하기" className="w-5" />
           Share
         </Button>
-        <Button className="bg-grayscale-600 text-grayscale-100 px-5 rounded-lg flex items-center gap-3">
+        <Button className="flex items-center gap-3 rounded-lg bg-grayscale-600 px-5 text-grayscale-100">
           <img src={exportIcon} alt="내보내기" className="w-5" />
           Export
         </Button>
       </div>
       <div className="flex gap-6">
-        <Button className="bg-transparent text-grayscale-100">로그인</Button>
-        <img src={profile} className="w-10 h-10" alt="프로필" />
+        <Button className="bg-transparent text-grayscale-100" onClick={openModal}>
+          로그인
+        </Button>
+        <img src={profile} className="h-10 w-10" alt="프로필" />
       </div>
+      <LoginModal open={open} close={closeModal} />
     </header>
   );
 }
