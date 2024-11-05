7448aaa158efb0009b71ebb417d4777753442328
Minhyung Cho <dup33333@hufs.ac.kr>
2024-11-05 21:58:50 +0900

    feat : useSection 커스텀 훅을 사용한 컨트롤 영역 제어

--- client/src/components/MainSection/ControlSection/index.tsx
+++ client/src/components/MainSection/ControlSection/index.tsx
@@ -1,9 +1,20 @@
 import ListView from "@/components/MainSection/ControlSection/ListView";
+import TextUpload from "@/components/MainSection/ControlSection/TextUpload";
+import VoiceFileUpload from "@/components/MainSection/ControlSection/VoiceFileUpload";
+import useSection from "@/hooks/useSection";
 
 export default function ControlSection() {
+  const mode = useSection().getmode() as keyof typeof modeView;
+  const modeView = {
+    voiceupload: <VoiceFileUpload />,
+    listview: <ListView />,
+    textupload: <TextUpload />,
+    default: <TextUpload />,
+  };
+
   return (
-    <section className="h-full w-1/3 min-w-80 rounded-[20px] bg-grayscale-700 p-8">
-      <ListView />
+    <section className="h-full w-1/3 min-w-80 max-w-[420px] rounded-[20px] bg-grayscale-700 p-8">
+      {modeView[mode] || modeView.default}
     </section>
   );
 }
