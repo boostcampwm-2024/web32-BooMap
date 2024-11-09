775510c0512d4400fa8ccb8cc9c0f49aaf230a08
Minhyung Cho <dup33333@hufs.ac.kr>
2024-11-09 17:26:49 +0900

    feat : ConnectedLine 좌표 계산 로직 함수 추가 getLinePoints -> Line의 props로 들어가는 points의 인자인 출발 x,y값과 도착 x,y값, 각 원의 반지름을 받아 두 원을 이어주는 선의 좌표를 return하는 함수 getCircleEdgePoint -> 각각의 원에 대해서 안쪽으로 선이 들어가지 않도록 반대편 원과 이어지면서 가장자리와 10px정도의 공백을 둔 좌표를 계산하는 함수

---
+++ client/src/utils/konva_mindmap/utils/points.ts
@@ -0,0 +1,36 @@
+import { Location } from "@/utils/konva_mindmap/types/location";
+
+// from 원의 중점에서 to 원의 중점으로 이어지는 선에서 from 선의 위치를 원의 중점이 아닌 가장자리의 위치 구하는 함수
+export function getCircleEdgePoint(from: Location, to: Location, fromRadius: number, toRadius: number) {
+  const dx = to.x - from.x;
+  const dy = to.y - from.y;
+
+  const vector = Math.sqrt(dx * dx + dy * dy);
+  if (fromRadius + toRadius > vector) {
+    return {
+      xEdgePoint: 0,
+      yEdgePoint: 0,
+    };
+  }
+
+  const xEdgePoint = Math.ceil(from.x + (fromRadius * dx) / vector);
+  const yEdgePoint = Math.ceil(from.y + (fromRadius * dy) / vector);
+
+  return {
+    xEdgePoint,
+    yEdgePoint,
+  };
+}
+
+// Point의 인자로 만들어주는 함수
+// 각각의 원에 대해서 가장자리 쪽으로 갈 수 있게끔 해줌
+export function getLinePoints(from: Location, to: Location, fromRadius: number, toRadius: number) {
+  const fromCircleEdgePoint = getCircleEdgePoint(from, to, fromRadius, toRadius);
+  const toCircleEdgePoint = getCircleEdgePoint(to, from, toRadius, fromRadius);
+  return [
+    fromCircleEdgePoint.xEdgePoint,
+    fromCircleEdgePoint.yEdgePoint,
+    toCircleEdgePoint.xEdgePoint,
+    toCircleEdgePoint.yEdgePoint,
+  ];
+}
