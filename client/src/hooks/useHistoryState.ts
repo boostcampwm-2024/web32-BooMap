64924f5f978b24ebe6893dd9cf698b7417a9bb3d
Minhyung Cho <dup33333@hufs.ac.kr>
2024-11-11 22:19:05 +0900

    fix : updateNodeList 로직이 상태변경을 제대로 하지 못하는 문제 수정

--- client/src/hooks/useHistoryState.ts
+++ client/src/hooks/useHistoryState.ts
@@ -1,36 +1,29 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback, useEffect } from "react";
 
 export default function useHistoryState<T>(initialState: T) {
-    const [data, _setData] = useState(initialState);
-    const [history, setHistory] = useState([initialState]);
-    const [pointer, setPointer] = useState(0);
+  const [data, _setData] = useState(initialState);
+  const [history, setHistory] = useState([initialState]);
+  const [pointer, setPointer] = useState(0);
 
-    const setData = useCallback(
-        (newState: T) =>
-            _setData(newState),
-        [pointer, history]
-    );
+  const setData = useCallback((newState) => _setData(newState), [pointer, history]);
 
-    const saveHistory = useCallback(
-        () => {
-            const newHistory = [...history.slice(0, pointer + 1), data];
-            setPointer(newHistory.length - 1);
-            setHistory(newHistory);
-        },
-        [pointer, history]
-    );
+  const saveHistory = useCallback(() => {
+    const newHistory = [...history.slice(0, pointer + 1), data];
+    setPointer(newHistory.length - 1);
+    setHistory(newHistory);
+  }, [pointer, history]);
 
-    const undo = useCallback(() => {
-        if (pointer <= 0) return;
-        setPointer((p) => p - 1);
-        _setData(history[pointer - 1]);
-    }, [history, pointer]);
+  const undo = useCallback(() => {
+    if (pointer <= 0) return;
+    setPointer((p) => p - 1);
+    _setData(history[pointer - 1]);
+  }, [history, pointer]);
 
-    const redo = useCallback(() => {
-        if (pointer >= history.length - 1) return;
-        setPointer((p) => p + 1);
-        _setData(history[pointer + 1]);
-    }, [history, pointer]);
+  const redo = useCallback(() => {
+    if (pointer >= history.length - 1) return;
+    setPointer((p) => p + 1);
+    _setData(history[pointer + 1]);
+  }, [history, pointer]);
 
-    return { data, setData, saveHistory, undo, redo };
+  return { data, setData, saveHistory, undo, redo };
 }
