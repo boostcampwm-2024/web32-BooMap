508d6fb50479fc16e1bb37ca130135b3cc32cd06
Minhyung Cho <dup33333@hufs.ac.kr>
2024-11-11 22:41:41 +0900

    chore : 사용하지 않는 import 삭제

--- client/src/hooks/useHistoryState.ts
+++ client/src/hooks/useHistoryState.ts
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect } from "react";
+import { useState, useCallback } from "react";
 
 export default function useHistoryState<T>(initialState: T) {
   const [data, _setData] = useState(initialState);
