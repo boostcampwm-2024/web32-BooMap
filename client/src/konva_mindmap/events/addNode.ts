a5c9b59adfbc0ef49d823b88761a38a29ea2e096
Minhyung Cho <dup33333@hufs.ac.kr>
2024-11-13 15:40:41 +0900

    feat : 노드 추가 로직 추가 수직 벡터를 구해 일정 값을 children의 마지막요소가 가지는 x,y좌표에 일정 값을 곱한 다음 더한 좌표값에서 새로운 노드가 생성되도록 조정 노드의 Id는 서버에서 받아와 넣어줄 예정이므로 아직까지 수정하지는 않았음

---
+++ client/src/konva_mindmap/events/addNode.ts
@@ -0,0 +1,56 @@
+import { unitVector } from "@/konva_mindmap/utils/vector";
+import { Node, NodeData } from "@/types/Node";
+
+export function addNode(data, selectedNode, overrideNodeData) {
+  if (!selectedNode || data[selectedNode.nodeId].depth === 3) return;
+
+  const newNodeId = parseInt(Object.keys(data)[Object.keys(data).length - 1]) + 1;
+  if (selectedNode.parentNodeId) {
+    overrideNodeData((prev) => ({
+      ...prev,
+      [selectedNode.nodeId]: {
+        ...data[selectedNode.nodeId],
+        children: [...data[selectedNode.nodeId].children, newNodeId],
+      },
+      [newNodeId]: {
+        id: newNodeId,
+        // keyword, depth, location 보내면 id 받고 id 받은걸로 상태 업데이트
+        keyword: "제목없음",
+        depth: data[selectedNode.parentNodeId].depth + 1,
+        location: getNewNodePosition(data[selectedNode.nodeId].children, data, data[selectedNode.nodeId]),
+        children: [],
+      },
+    }));
+    return;
+  }
+
+  overrideNodeData((prev) => ({
+    ...prev,
+    [selectedNode.nodeId]: {
+      ...data[selectedNode.nodeId],
+      children: [...data[selectedNode.nodeId].children, newNodeId],
+    },
+    [newNodeId]: {
+      id: newNodeId,
+      keyword: "제목없음",
+      depth: data[selectedNode.nodeId].depth + 1,
+      location: getNewNodePosition(data[selectedNode.nodeId].children, data, data[selectedNode.nodeId]),
+      children: [],
+    },
+  }));
+}
+
+// 수직벡터 -> 벡터 구한 다음에 벡터의 y값이 x값으로 가고 x값의 반대 부호값이 y좌표
+// 단위벡터 * 내가 원하는 만큼 떼놓을 거리값을 마지막 요소의 x와 y좌표에 더한다,
+function getNewNodePosition(children: number[], data: NodeData, parentNode: Node) {
+  if (!children.length) {
+    return parentNode ? { x: parentNode.location.x + 30, y: parentNode.location.y + 30 } : { x: 0, y: 0 };
+  }
+  const lastChildren = data[children[children.length - 1]];
+  const uv = unitVector(parentNode.location, lastChildren.location);
+
+  return {
+    x: lastChildren.location.x + uv.x * 50,
+    y: lastChildren.location.y + uv.y * 50,
+  };
+}
