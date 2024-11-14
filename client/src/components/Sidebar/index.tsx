fb637f1a627d2f451117edbd686c3a25b3f0fa49
minju <alswn1583@naver.com>
2024-11-14 11:00:52 +0900

    feat : 로고 눌렀을 때 초기 url로 이동

--- client/src/components/Sidebar/index.tsx
+++ client/src/components/Sidebar/index.tsx
@@ -1,10 +1,17 @@
 import logo from "@/assets/logo.png";
 import Overview from "@/components/Sidebar/Overview";
+import { useNavigate } from "react-router-dom";
+useNavigate;
 
 export default function Sidebar() {
+  const navigate = useNavigate();
+
   return (
     <aside className="flex h-full min-w-64 flex-col bg-grayscale-800 p-4">
-      <div className="flex w-full items-center justify-center gap-4 text-2xl font-black">
+      <div
+        className="flex w-full cursor-pointer items-center justify-center gap-4 text-2xl font-black"
+        onClick={() => navigate("/")}
+      >
         <img src={logo} alt="로고" className="w-12" />
         BOOMAP
       </div>
