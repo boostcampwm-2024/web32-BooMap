ca473b4856192c8f29c3c93537bbcc780a0774d7
adkm12 <adkm@gachon.ac.kr>
2024-11-19 01:33:41 +0900

    feat: 사용자 모듈 추가
    - github 유저 생성 및 이메일 조회
    - userId로 조회

---
+++ api-server/src/modules/user/user.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UserService {
+  constructor(@InjectRepository(User) private userRepository: Repository<User>) {}
+
+  async createGithubUser(email: string) {
+    const user = this.userRepository.create({ email, type: 'github' });
+    return await this.userRepository.save(user);
+  }
+
+  async findByGithubEmail(email: string) {
+    return await this.userRepository.findOne({ where: { email, type: 'github' } });
+  }
+
+  async findById(id: number) {
+    return await this.userRepository.findOne({ where: { id } });
+  }
+}
