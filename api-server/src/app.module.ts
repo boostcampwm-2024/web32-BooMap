c61bd271f7635f5ade79ca65b45734105d923f94
adkm12 <adkm@gachon.ac.kr>
2024-11-19 01:34:57 +0900

    feat: 인증 모듈 추가 및 JWT 전략 구현
    - github 로그인 구현
    - passport 적용
    - JWT 적용
    - refresh api 작성
    - 로그아웃 기능 추가

--- api-server/src/app.module.ts
+++ api-server/src/app.module.ts
@@ -3,11 +3,14 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { WinstonModule } from 'nest-winston';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RedisModule } from '@liaoliaots/nestjs-redis';
-import { getWinstonConfig, getTypeOrmConfig, getRedisConfig } from './configs';
+import { getWinstonConfig, getTypeOrmConfig, getRedisConfig, getJwtConfig } from './configs';
 import { TestModule } from './modules/test/test.module';
 import { ConnectionModule } from './modules/connection/connection.module';
 
 import { LoggerMiddleware } from './middlewares/logger.middleware';
+import { AuthModule } from './modules/auth/auth.module';
+import { UserModule } from './modules/user/user.module';
+import { JwtModule } from '@nestjs/jwt';
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -26,8 +29,14 @@ import { LoggerMiddleware } from './middlewares/logger.middleware';
       inject: [ConfigService],
       useFactory: getTypeOrmConfig,
     }),
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: getJwtConfig,
+    }),
     TestModule,
     ConnectionModule,
+    UserModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
