eea10c11f6fbe1a8d21baae2b35e0c2bb4d4b65b
minju <alswn1583@naver.com>
2024-11-18 18:01:27 +0900

    feature : 오타 수정 & openAccordian함수 추가

--- client/src/hooks/useAccordian.ts
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-
-export default function useAccordian() {
-  const [open, setOpen] = useState(false);
-  function handleAccordian() {
-    setOpen(!open);
-  }
-  return { open, handleAccordian };
-}
