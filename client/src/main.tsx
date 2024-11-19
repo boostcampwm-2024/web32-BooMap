03a2c8ef8433efc5dbc354a7adefa9ce20d9ec67
Minhyung Cho <dup33333@hufs.ac.kr>
2024-11-19 23:21:31 +0900

    feat : authorization 관련 callback 등록 및 react-query client&Provider 세팅

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,15 +3,39 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import NotFound from "@/components/common/NotFound";
+import AuthorizeCallback from "@/pages/auth";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
   },
+  {
+    path: "/auth/github/callback",
+    element: <AuthorizeCallback />,
+  },
+  {
+    path: "/auth/kakao/callback",
+    element: <AuthorizeCallback />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
 ]);
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      throwOnError: true,
+    },
+  },
+});
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>,
 );
