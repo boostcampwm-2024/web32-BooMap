ce827912033a3a261fff25653657897ce0af8d17
Minhyung Cho <dup33333@hufs.ac.kr>
2024-11-19 23:54:22 +0900

    feat : getUser api 추가

--- client/src/api/auth.ts
+++ client/src/api/auth.ts
@@ -1,7 +1,12 @@
 import { instance } from "@/api";
-import { TokenRefresh } from "@/types/auth";
+import { TokenRefresh, User } from "@/types/auth";
 
 export const tokenRefresh = async (): Promise<TokenRefresh> => {
-  const { data } = await instance.post("/api/auth/refresh", {}, { withCredentials: true });
+  const { data } = await instance.post("/api/auth/refresh");
+  return data;
+};
+
+export const getUser = async (): Promise<User> => {
+  const { data } = await instance.get("/api/user/info");
   return data;
 };
---
+++ client/src/types/auth.ts
@@ -0,0 +1,7 @@
+export type TokenRefresh = {
+  accessToken: string;
+};
+
+export type User = {
+  email: string;
+};
