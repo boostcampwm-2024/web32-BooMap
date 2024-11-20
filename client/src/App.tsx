5f7cd08b01a93f1c8cdd228835ef1954732bf91a
Minhyung Cho <dup33333@hufs.ac.kr>
2024-11-20 20:07:05 +0900

    feat : 엔트리 포인트에 suspense와 Error Boundary 처리

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,16 @@
 import MainPage from "@/pages/Main";
 import "./App.css";
+import { Suspense } from "react";
+import Spinner from "@/components/common/Spinner";
+import { ErrorBoundary } from "react-error-boundary";
+import NotFound from "@/components/common/NotFound";
 
 export default function App() {
-  return <MainPage />;
+  return (
+    <ErrorBoundary fallback={<NotFound />}>
+      <Suspense fallback={<Spinner />}>
+        <MainPage />;
+      </Suspense>
+    </ErrorBoundary>
+  );
 }
