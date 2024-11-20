5e7e0261c135a6d806f692fbb1fc9ced04054470
kimnamheeee <nami200284@snu.ac.kr>
2024-11-20 21:04:13 +0900

    refactor : 애니메이션 관련 클래스명 변수로 분리

--- client/src/components/Sidebar/index.tsx
+++ client/src/components/Sidebar/index.tsx
@@ -11,12 +11,13 @@ type SidebarProps = {
 export default function Sidebar({ isSidebarOpen, toggleSidebar }: SidebarProps) {
   const navigate = useNavigate();
 
+  const animateOptions = isSidebarOpen ? "translate-x-0 opacity-100" : "-translate-x-full opacity-0";
+  const transitionClasses = "transition-all duration-300 ease-in-out";
+
   return (
     <>
       <aside
-        className={`absolute left-0 top-0 h-full min-w-64 transform bg-grayscale-800 p-4 transition-all duration-300 ease-in-out ${
-          isSidebarOpen ? "translate-x-0 opacity-100" : "-translate-x-full opacity-0"
-        }`}
+        className={`absolute left-0 top-0 h-full min-w-64 transform bg-grayscale-800 p-4 ${transitionClasses} ${animateOptions}`}
       >
         <div
           className="flex w-full cursor-pointer items-center justify-center gap-4 text-2xl font-black"
