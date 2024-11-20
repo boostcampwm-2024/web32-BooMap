a93343dc94e016782b0a8ad436adcf0c68e2b6b0
Minhyung Cho <dup33333@hufs.ac.kr>
2024-11-21 00:25:39 +0900

    remove : 메인 페이지 suspense 제거

--- client/src/pages/auth/index.tsx
+++ client/src/pages/auth/index.tsx
@@ -1,15 +1,11 @@
 import Authorize from "@/components/Authorize";
 import NotFound from "@/components/common/NotFound";
-import Spinner from "@/components/common/Spinner";
-import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 
 export default function AuthorizeCallback() {
   return (
     <ErrorBoundary fallback={<NotFound />}>
-      <Suspense fallback={<Spinner />}>
-        <Authorize />
-      </Suspense>
+      <Authorize />
     </ErrorBoundary>
   );
 }
