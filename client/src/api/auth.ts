22a09eeaa48932aa453bbbbd9fff3515a64d5dd4
Minhyung Cho <dup33333@hufs.ac.kr>
2024-11-21 00:22:43 +0900

    feat : 로그인 테스트용 함수 추가

--- client/src/api/auth.ts
+++ client/src/api/auth.ts
@@ -1,12 +1,22 @@
 import { instance } from "@/api";
 import { TokenRefresh, User } from "@/types/auth";
+import { setToken } from "@/utils/token";
 
 export const tokenRefresh = async (): Promise<TokenRefresh> => {
-  const { data } = await instance.post("/api/auth/refresh");
-  return data;
+  const { data } = await instance.post("/auth/refresh");
+  setToken(data.accessToken);
+  return;
 };
 
 export const getUser = async (): Promise<User> => {
-  const { data } = await instance.get("/api/user/info");
-  return data;
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({ email: "qwrqwrqwr@naver.com", nickname: "민형" });
+    }, 2000);
+  });
 };
+
+// export const getUser = async (): Promise<User> => {
+//   const { data } = await instance.get("/user/info");
+//   return data;
+// };
