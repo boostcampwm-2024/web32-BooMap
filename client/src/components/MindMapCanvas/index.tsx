597570c7bf50e8426096a3b8215f110eaab26c43
kimnamheeee <nami200284@snu.ac.kr>
2024-11-21 14:20:06 +0900

    refactor : 1이 아닌 함수 반환값으로 루트노드 key 설정

--- client/src/components/MindMapCanvas/index.tsx
+++ client/src/components/MindMapCanvas/index.tsx
@@ -10,6 +10,7 @@ import { useEffect, useRef } from "react";
 import { useStageStore } from "@/store/useStageStore";
 import NoNodeInform from "@/components/MindMapCanvas/NoNodeInform";
 import CanvasButtons from "@/components/MindMapCanvas/CanvasButtons";
+import { findRootNodeKey } from "@/konva_mindmap/utils/findRootNodeKey";
 
 export default function MindMapCanvas({ showMinutes, handleShowMinutes }) {
   const { data, undoData: undo, redoData: redo, updateNode, overrideNodeData, saveHistory } = useNodeListContext();
@@ -17,6 +18,8 @@ export default function MindMapCanvas({ showMinutes, handleShowMinutes }) {
   const registerLayer = useCollisionDetection(data, updateNode);
   const stageRef = useRef();
   const { registerStageRef } = useStageStore();
+
+  const rootKey = findRootNodeKey(data);
 
   useEffect(() => {
     registerStageRef(stageRef);
@@ -53,7 +56,7 @@ export default function MindMapCanvas({ showMinutes, handleShowMinutes }) {
         onWheel={handleWheel}
       >
         <Layer ref={registerLayer}>
-          {Object.keys(data).length >= 1 && <DrawMindMap data={data} root={data[1]} depth={1} />}
+          {Object.keys(data).length >= 1 && <DrawMindMap data={data} root={data[rootKey]} depth={1} />}
         </Layer>
       </Stage>
       <ToolMenu dimensions={dimensions} zoomIn={zoomIn} zoomOut={zoomOut} />
