02ea9d5a053289e497063d62ec53bdb88c7908b3
Minhyung Cho <dup33333@hufs.ac.kr>
2024-11-21 17:34:19 +0900

    chore : auth에서 에러 수정 및 로딩 상태 추가와 토큰 리프레시 커스텀 훅으로 변경

--- client/src/components/Authorize/index.tsx
+++ client/src/components/Authorize/index.tsx
@@ -1,24 +1,23 @@
 import { tokenRefresh } from "@/api/auth";
+import Spinner from "@/components/common/Spinner";
+import { useTokenRefresh } from "@/hooks/useTokenRefresh";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { useEffect } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 
 export default function Authorize() {
   const [searchParams] = useSearchParams();
-  const loginSuccess = searchParams.get("success");
+  // const loginSuccess = searchParams.get("success");
   const navigate = useNavigate();
 
-  useSuspenseQuery({
-    queryKey: ["auth"],
-    queryFn: tokenRefresh,
-    retry: 2,
-  });
+  const { isPending } = useTokenRefresh();
 
   useEffect(() => {
-    if (loginSuccess === "true") {
-      navigate("/");
-    }
-  }, [loginSuccess, navigate]);
+    // if (loginSuccess === "true") {
+    navigate("/");
+    // }
+  }, [navigate]);
 
+  if (isPending) return <Spinner />;
   return <p className="text-white">Authenticated</p>;
 }
