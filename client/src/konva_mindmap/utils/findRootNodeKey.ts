e5f5356780a2d7415a78f2f011a5326b2c32395b
kimnamheeee <nami200284@snu.ac.kr>
2024-11-21 15:34:45 +0900

    refactor : findRootNodeKey 함수 에러 수정

--- client/src/konva_mindmap/utils/findRootNodeKey.ts
+++ client/src/konva_mindmap/utils/findRootNodeKey.ts
@@ -1,5 +1,5 @@
 import { NodeData } from "@/types/Node";
 
 export function findRootNodeKey(data: NodeData) {
-  return Number(Object.keys(data).find((key) => data[key].depth === 1)[1]);
+  return Number(Object.keys(data).find((key) => data[key].depth === 1));
 }
