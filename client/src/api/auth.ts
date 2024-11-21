36c1e4a6ae828e5579dddf195278e9c2d7d0f332
Minhyung Cho <dup33333@hufs.ac.kr>
2024-11-21 17:35:21 +0900

    remove : getUser 테스트용 함수 삭제

--- client/src/api/auth.ts
+++ client/src/api/auth.ts
@@ -3,20 +3,20 @@ import { TokenRefresh, User } from "@/types/auth";
 import { setToken } from "@/utils/token";
 
 export const tokenRefresh = async (): Promise<TokenRefresh> => {
-  const { data } = await instance.post("/auth/refresh");
+  const { data } = await instance.post("auth/refresh", {}, { withCredentials: true });
   setToken(data.accessToken);
   return;
 };
 
-export const getUser = async (): Promise<User> => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve({ email: "qwrqwrqwr@naver.com", nickname: "민형" });
-    }, 2000);
-  });
-};
-
 // export const getUser = async (): Promise<User> => {
-//   const { data } = await instance.get("/user/info");
-//   return data;
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve({ email: "qwrqwrqwr@naver.com", nickname: "민형" });
+//     }, 2000);
+//   });
 // };
+
+export const getUser = async (): Promise<User> => {
+  const { data } = await instance.get("user/info");
+  return data;
+};
