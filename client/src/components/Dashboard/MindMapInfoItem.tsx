70c06073e9611b165fcb53e0d11e71e476bca376
minju <alswn1583@naver.com>
2024-11-21 17:29:41 +0900

    feat : 대시보드 삭제 & 검색 기능 구현

--- client/src/components/Dashboard/MindMapInfoItem.tsx
+++ client/src/components/Dashboard/MindMapInfoItem.tsx
@@ -3,12 +3,31 @@ import deleteIcon from "@/assets/trash2.png";
 import useModal from "@/hooks/useModal";
 import { Button } from "@headlessui/react";
 import Modal from "../common/Modal";
+import extractDate from "@/utils/extractDate";
 
-export default function MindMapInfoItem({ data, index }) {
+interface MindMapInfoItemProps {
+  data: {
+    id: number;
+    connectionId: string;
+    title: string;
+    keyword: string[];
+    createdDate: Date;
+    modifiedDate: Date;
+    owner: string;
+  };
+  index: number;
+  handleDeleteData: (id: number) => void;
+}
+
+export default function MindMapInfoItem({ data, index, handleDeleteData }: MindMapInfoItemProps) {
   const { open, openModal, closeModal } = useModal();
 
+  const keywordData = data.keyword.slice(0, 4);
+
+  const ownerCheck = localStorage.getItem("user") === data.owner;
+
   const confirmDelete = () => {
-    console.log("마인드 맵 삭제 확인");
+    handleDeleteData(data.id);
     closeModal();
   };
 
@@ -18,31 +37,38 @@ export default function MindMapInfoItem({ data, index }) {
         className={`${index !== 0 ? "border-t-[1px] border-t-grayscale-500" : ""} flex items-center justify-between px-3 py-2`}
       >
         <div className="min-w-72">{data.title}</div>
-        <div className="grid w-44 grid-cols-2 gap-1 px-5 text-xs">
-          {data.keyword.map((keyword, i) => (
-            <span key={i} className="rounded-2xl bg-bm-purple px-4 py-[2px] text-center">
+        <div className="grid w-60 grid-cols-2 gap-1 px-5 text-xs">
+          {keywordData.map((keyword, i) => (
+            <span
+              key={i}
+              className="overflow-hidden text-ellipsis whitespace-nowrap rounded-2xl bg-bm-purple px-4 py-1 text-center"
+            >
               {keyword}
             </span>
           ))}
         </div>
         <div className="flex min-w-24 items-center justify-center gap-2">
           <img className="h-6 w-6" src={profile} alt="소유자 이미지" />
-          <div>{data.owner.id}</div>
+          <div>{data.owner}</div>
         </div>
         <div className="flex min-w-40 justify-between">
-          <div>{data.createdAt}</div>
-          <Button className="group" onClick={openModal}>
-            <img
-              className="h-6 w-6 group-hover:brightness-0 group-hover:invert"
-              src={deleteIcon}
-              alt="마인드 맵 삭제 버튼"
-            />
-          </Button>
+          <div>{extractDate(data.createdDate)}</div>
+          {ownerCheck ? (
+            <Button className="group" onClick={openModal}>
+              <img
+                className="h-6 w-6 group-hover:brightness-0 group-hover:invert"
+                src={deleteIcon}
+                alt="마인드 맵 삭제 버튼"
+              />
+            </Button>
+          ) : (
+            <></>
+          )}
         </div>
       </div>
       <Modal open={open} closeModal={closeModal}>
         <p className="mb-4 text-center text-lg text-black">
-          <span className="font-bold">{"갱"}</span>
+          <span className="font-bold">{data.title}</span>
           <br></br>마인드 맵을 삭제하시겠습니까?
         </p>
         <div className="flex justify-center gap-2">
--- client/src/components/Dashboard/UserDashBoard.tsx
+++ client/src/components/Dashboard/UserDashBoard.tsx
@@ -2,26 +2,151 @@ import MindMapInfoItem from "./MindMapInfoItem";
 import addIcon from "@/assets/whitePlus.png";
 import searchIcon from "@/assets/search.png";
 import { Button, Input } from "@headlessui/react";
+import { useState } from "react";
+
+const apiData = {
+  mindMaps: [
+    {
+      id: 1,
+      connectionId: "asdf12-asd1f8-asd1f5-asdf",
+      title: "점심 메뉴를 골라볼까나~?",
+      keyword: ["점심메뉴", "한식", "중식", "양식", "일식", "피자", "초밥"],
+      createdDate: new Date(),
+      modifiedDate: new Date(),
+      owner: "강민주",
+    },
+    {
+      id: 2,
+      connectionId: "asdf12-asd1f8-asd1f5-asdf",
+      title: "이번 주 주말에 뭐할까?",
+      keyword: ["주말에 갈 곳", "놀이공원", "더현대 서울", "카페", "쇼핑", "놀이기구"],
+      createdDate: new Date(),
+      modifiedDate: new Date(),
+      owner: "양현호",
+    },
+    {
+      id: 3,
+      connectionId: "asdf12-asd1f8-asd1f5-asdf",
+      title: "여행",
+      keyword: ["여행", "국내", "국외", "부산", "제주도", "여수", "일본", "베트남", "미국"],
+      createdDate: new Date(),
+      modifiedDate: new Date(),
+      owner: "조민형",
+    },
+    {
+      id: 4,
+      connectionId: "asdf12-asd1f8-asd1f5-asdf",
+      title: "프로젝트 회의",
+      keyword: ["회의", "에러 발생!!", "추가 기능", "진행상황공유", "UI 에러..."],
+      createdDate: new Date(),
+      modifiedDate: new Date(),
+      owner: "김남희",
+    },
+    {
+      id: 5,
+      connectionId: "asdf12-asd1f8-asd1f5-asdf",
+      title: "점심 메뉴를 골라볼까나~?",
+      keyword: ["점심메뉴", "한식", "중식", "양식", "일식", "피자", "초밥"],
+      createdDate: new Date(),
+      modifiedDate: new Date(),
+      owner: "강민주",
+    },
+    {
+      id: 6,
+      connectionId: "asdf12-asd1f8-asd1f5-asdf",
+      title: "이번 주 주말에 뭐할까?",
+      keyword: ["주말에 갈 곳", "놀이공원", "더현대 서울", "카페", "쇼핑", "놀이기구"],
+      createdDate: new Date(),
+      modifiedDate: new Date(),
+      owner: "양현호",
+    },
+    {
+      id: 7,
+      connectionId: "asdf12-asd1f8-asd1f5-asdf",
+      title: "여행",
+      keyword: ["여행", "국내", "국외", "부산", "제주도", "여수", "일본", "베트남", "미국"],
+      createdDate: new Date(),
+      modifiedDate: new Date(),
+      owner: "조민형",
+    },
+    {
+      id: 8,
+      connectionId: "asdf12-asd1f8-asd1f5-asdf",
+      title: "프로젝트 회의",
+      keyword: ["회의", "에러 발생!!", "추가 기능", "진행상황공유", "UI 에러..."],
+      createdDate: new Date(),
+      modifiedDate: new Date(),
+      owner: "김남희",
+    },
+    {
+      id: 9,
+      connectionId: "asdf12-asd1f8-asd1f5-asdf",
+      title: "점심 메뉴를 골라볼까나~?",
+      keyword: ["점심메뉴", "한식", "중식", "양식", "일식", "피자", "초밥"],
+      createdDate: new Date(),
+      modifiedDate: new Date(),
+      owner: "강민주",
+    },
+    {
+      id: 10,
+      connectionId: "asdf12-asd1f8-asd1f5-asdf",
+      title: "이번 주 주말에 뭐할까?",
+      keyword: ["주말에 갈 곳", "놀이공원", "더현대 서울", "카페", "쇼핑", "놀이기구"],
+      createdDate: new Date(),
+      modifiedDate: new Date(),
+      owner: "양현호",
+    },
+    {
+      id: 11,
+      connectionId: "asdf12-asd1f8-asd1f5-asdf",
+      title: "여행",
+      keyword: ["여행", "국내", "국외", "부산", "제주도", "여수", "일본", "베트남", "미국"],
+      createdDate: new Date(),
+      modifiedDate: new Date(),
+      owner: "조민형",
+    },
+    {
+      id: 12,
+      connectionId: "asdf12-asd1f8-asd1f5-asdf",
+      title: "프로젝트 회의",
+      keyword: ["회의", "에러 발생!!", "추가 기능", "진행상황공유", "UI 에러..."],
+      createdDate: new Date(),
+      modifiedDate: new Date(),
+      owner: "김남희",
+    },
+  ],
+};
 
 export default function UserDashBoard() {
-  const data = Array(10).fill({
-    title: "어쩌구 제목 마인드맵",
-    keyword: ["대분류", "중분류", "분류"],
-    owner: { img: "이미지", id: "user1" },
-    createdAt: "2024.11.13",
-  });
+  const [data, setData] = useState(apiData.mindMaps);
+  const [searchContent, setSearchContent] = useState("");
+  localStorage.setItem("user", "강민주");
+
+  function handleDeleteData(id: number) {
+    const newData = data.filter((item) => item.id !== id);
+    setData(newData);
+  }
+
+  function searchData(content: string) {
+    const filteredData = data.filter(
+      (item) => item.title.includes(content) || item.keyword.some((k) => k.includes(content)),
+    );
+    return filteredData;
+  }
+
+  const filteredData = searchData(searchContent);
 
   return (
     <div className="relative flex h-full w-full flex-col rounded-[20px] bg-grayscale-700 px-8 pb-24 pt-8">
       <header className="flex items-center justify-between px-3 py-2 font-bold">
         <div className="min-w-72 pl-2">제목</div>
-        <div className="min-w-44 pl-8">키워드</div>
+        <div className="min-w-60 pl-8">키워드</div>
         <div className="min-w-24 pl-4">소유자</div>
         <div className="min-w-40 pl-1">생성 일자</div>
       </header>
       <div className="no-scrollbar h-[calc(100%-40px)] overflow-y-scroll border-b-[1px] border-t-[1px] border-grayscale-500">
-        {data.map((v, i) => (
-          <MindMapInfoItem key={i} data={v} index={i} />
+        {filteredData.map((info, i) => (
+          <MindMapInfoItem key={i} data={info} index={i} handleDeleteData={handleDeleteData} />
         ))}
       </div>
       <div className="absolute bottom-8 right-8">
@@ -34,8 +159,14 @@ export default function UserDashBoard() {
         </Button>
       </div>
       <div className="absolute right-0 top-[-47px] flex items-center gap-2 rounded-2xl bg-grayscale-700 px-4 py-2 text-grayscale-200">
-        <Input className="w-48 bg-inherit text-xs focus:outline-none" placeholder="키워드나 제목을 입력하세요" />
-        <Button onClick={() => console.log("키워드나 제목으로 검색하기")}>
+        <Input
+          className="w-48 bg-inherit text-xs focus:outline-none"
+          onChange={(e) => {
+            setSearchContent(e.target.value);
+          }}
+          placeholder="키워드나 제목을 입력하세요"
+        />
+        <Button onClick={() => searchData(searchContent)}>
           <img className="h-6 w-6" src={searchIcon} alt="검색하기 버튼" />
         </Button>
       </div>
