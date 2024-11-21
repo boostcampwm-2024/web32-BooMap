572ba81c63c5d7b94addad77a7536f5b7d704c52
adkm12 <adkm@gachon.ac.kr>
2024-11-22 00:30:47 +0900

    fix : redirect URL 추가, jwtStrategy provider 추가

--- BE/apps/api-server/src/modules/auth/auth.module.ts
+++ BE/apps/api-server/src/modules/auth/auth.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PassportModule } from '@nestjs/passport';
-import { KakaoStrategy, GithubStrategy } from './strategies';
+import { KakaoStrategy, GithubStrategy, JwtStrategy } from './strategies';
 import { UserModule } from '../user/user.module';
 
 @Module({
   imports: [PassportModule, UserModule],
   controllers: [AuthController],
-  providers: [AuthService, GithubStrategy, KakaoStrategy],
+  providers: [AuthService, GithubStrategy, KakaoStrategy, JwtStrategy],
 })
 export class AuthModule {}
