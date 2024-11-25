a74b29118fefd237189d56b44912fea9ce452704
Minhyung Cho <dup33333@hufs.ac.kr>
2024-11-25 21:32:20 +0900

    refactor : 마인드맵 커맨드 키별 움직임 변경

--- client/src/konva_mindmap/components/MindMapNode.tsx
+++ client/src/konva_mindmap/components/MindMapNode.tsx
@@ -1,29 +1,26 @@
+import NewNode from "@/konva_mindmap/components/NewNode";
 import EditableText from "@/konva_mindmap/components/EditableText";
 import { NodeProps } from "@/types/Node";
 import { Circle, Group } from "react-konva";
 import { useNodeListContext } from "@/store/NodeListProvider";
-import { useState } from "react";
+import React, { useRef, useState } from "react";
 import { SocketSlice } from "@/store/SocketSlice";
 import { checkFollowing, reconcileOffsets, resetSavedOffsets, saveOffsets } from "@/konva_mindmap/utils/following";
-import NewNode from "@/konva_mindmap/components/NewNode";
 import { colors } from "@/constants/color";
+import ConnectedLine from "@/konva_mindmap/components/ConnectedLine";
+import Konva from "konva";
+import { getMovedNodesLocation } from "@/konva_mindmap/utils/select";
 
-export default function MindMapNode({ data, parentNode, node, depth }: NodeProps) {
-  if (node.newNode) return <NewNode data={data} parentNode={parentNode} node={node} depth={depth} />;
-  const { saveHistory, updateNode, selectNode, selectedNode } = useNodeListContext();
+export default function MindMapNode({ data, parentNode, node, depth, parentRef, dragmode }: NodeProps) {
+  if (node.newNode)
+    return <NewNode data={data} parentNode={parentNode} node={node} depth={depth} dragmode={dragmode} />;
+  const nodeRef = useRef<Konva.Group>(null);
+  const { saveHistory, updateNode, selectNode, selectedNode, selectedGroup, overrideNodeData } = useNodeListContext();
   const [isEditing, setIsEditing] = useState(false);
   const socket = SocketSlice.getState().socket;
 
   function handleDoubleClick() {
     setIsEditing(true);
-  }
-
-  function handleMouseEnter() {
-    this.setStroke("red");
-  }
-
-  function handleMouseLeave() {
-    this.setStroke("");
   }
 
   function handleDragEnd() {
@@ -46,50 +43,93 @@ export default function MindMapNode({ data, parentNode, node, depth }: NodeProps
     }
     selectNode({ nodeId: node.id, parentNodeId: parentNode ? parentNode.id : null });
   }
+
+  const NodeStroke = selectedGroup.includes(node.id.toString()) ? "red" : "";
+
+  const handleDragMove = (e: Konva.KonvaEventObject<DragEvent>) => {
+    const currentPos = { x: e.target.x(), y: e.target.y() };
+    const dx = currentPos.x - node.location.x;
+    const dy = currentPos.y - node.location.y;
+
+    if (selectedGroup.length) {
+      const groupMove = getMovedNodesLocation(data, selectedGroup, node, dx, dy, currentPos);
+      overrideNodeData(groupMove);
+      return;
+    }
+
+    updateNode(node.id, {
+      ...node,
+      location: currentPos,
+    });
+
+    if (e.evt.ctrlKey || e.evt.metaKey) {
+      checkFollowing(data, node, currentPos, updateNode);
+    }
+
+    // 현재 노드 업데이트
+  };
+
   return (
-    <Group
-      onDblClick={handleDoubleClick}
-      name="node"
-      id={node.id.toString()}
-      onDragStart={() => {
-        saveOffsets(data, node);
-      }}
-      onDragMove={(e) => {
-        updateNode(node.id, {
-          ...node,
-          location: {
-            x: e.target.x(),
-            y: e.target.y(),
-          },
-        });
-        checkFollowing(data, node, updateNode);
-      }}
-      onDragEnd={handleDragEnd}
-      draggable
-      x={node.location.x}
-      y={node.location.y}
-      onClick={handleClick}
-    >
-      <Circle
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
-        fill={selectedNode?.nodeId === node.id ? "orange" : colors[depth - 1]}
-        width={100}
-        height={100}
-        strokeWidth={3}
-        radius={60 - depth * 10}
-        shadowBlur={5}
-      />
-      <EditableText
-        id={node.id}
-        name="text"
-        text={node.keyword}
-        offsetX={70 - depth * 10}
-        offsetY={8 * depth - 60}
-        width={140 - depth * 20}
-        isEditing={isEditing}
-        setIsEditing={setIsEditing}
-      />
-    </Group>
+    <>
+      <Group
+        ref={nodeRef}
+        onDblClick={handleDoubleClick}
+        name="node"
+        id={node.id.toString()}
+        onDragStart={() => {
+          saveOffsets(data, node);
+        }}
+        onDragMove={handleDragMove}
+        onDragEnd={() => {
+          // reconcileOffsets(data, node, updateNode);
+          resetSavedOffsets();
+          saveHistory(JSON.stringify(data));
+        }}
+        draggable={!dragmode}
+        x={node.location.x}
+        y={node.location.y}
+        onClick={handleClick}
+      >
+        <Circle
+          stroke={NodeStroke}
+          fill={selectedNode?.nodeId === node.id ? "orange" : colors[depth - 1]}
+          width={100}
+          height={100}
+          strokeWidth={3}
+          radius={60 - depth * 10}
+          shadowBlur={5}
+        />
+        <EditableText
+          id={node.id}
+          name="text"
+          text={node.keyword}
+          offsetX={70 - depth * 10}
+          offsetY={8 * depth - 60}
+          width={140 - depth * 20}
+          isEditing={isEditing}
+          setIsEditing={setIsEditing}
+        />
+      </Group>
+      {parentNode && (
+        <ConnectedLine
+          from={parentNode.location}
+          to={node.location}
+          fromRadius={70 - depth * 10}
+          toRadius={60 - depth * 10}
+        />
+      )}
+      {node.children?.map((childNode, index) => (
+        <React.Fragment key={index}>
+          <MindMapNode
+            data={data}
+            node={data[childNode]}
+            depth={depth + 1}
+            parentNode={node}
+            parentRef={nodeRef}
+            dragmode={dragmode}
+          />
+        </React.Fragment>
+      ))}
+    </>
   );
 }
