feee07a43dbb661a688aa58ec6e50def022b82b7
kimnamheeee <nami200284@snu.ac.kr>
2024-11-25 22:55:10 +0900

    refactor : 새로고침시 소켓 재연결 로직 복구
    conflict 해결 이후 사라진 로직을 찾아...

--- client/src/components/MindMapMainSection/index.tsx
+++ client/src/components/MindMapMainSection/index.tsx
@@ -2,6 +2,9 @@ import NotFound from "@/components/common/NotFound";
 import MindMapHeader from "@/components/MindMapHeader";
 import MindMapView from "@/components/MindMapMainSection/MindMapView";
 import useSection from "@/hooks/useSection";
+import { SocketSlice } from "@/store/SocketSlice";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 const modeView = {
   voiceupload: "음성 파일 업로드",
@@ -11,6 +14,15 @@ const modeView = {
 
 export default function MindMapMainSection() {
   const mode = useSection().searchParams.get("mode") as keyof typeof modeView;
+  const { mindMapId } = useParams<{ mindMapId: string }>();
+  const { connectSocket, disconnectSocket } = SocketSlice();
+
+  useEffect(() => {
+    if (mindMapId) connectSocket(mindMapId);
+    return () => {
+      disconnectSocket();
+    };
+  }, [mindMapId, connectSocket, disconnectSocket]);
 
   if (!modeView[mode]) return <NotFound />;
 
