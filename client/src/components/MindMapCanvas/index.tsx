a0321ba9dca86589cd6f361fb4801fe53763c52e
Minhyung Cho <dup33333@hufs.ac.kr>
2024-11-25 21:30:11 +0900

    feat : 키 매핑 및 ToolMenu에 드래그모드 추가

--- client/src/components/MindMapCanvas/index.tsx
+++ client/src/components/MindMapCanvas/index.tsx
@@ -1,15 +1,18 @@
 import useDimension from "@/konva_mindmap/hooks/useDimension";
 import useWindowKeyEventListener from "@/hooks/useWindowKeyEventListener";
-import DrawMindMap from "@/konva_mindmap/components/DrawMindMap";
 import ToolMenu from "@/components/MindMapCanvas/ToolMenu";
 import initializeNodePosition from "@/konva_mindmap/utils/initializeNodePosition";
 import { Layer, Stage } from "react-konva";
 import { useNodeListContext } from "@/store/NodeListProvider";
 import { useCollisionDetection } from "@/konva_mindmap/hooks/useCollisionDetection";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useStageStore } from "@/store/useStageStore";
 import NoNodeInform from "@/components/MindMapCanvas/NoNodeInform";
 import CanvasButtons from "@/components/MindMapCanvas/CanvasButtons";
+import MindMapNode from "@/konva_mindmap/components/MindMapNode";
+import Konva from "konva";
+import SelectionRect from "@/konva_mindmap/components/selectionRect";
+import DrawMindMap from "@/konva_mindmap/components/DrawMindMap";
 import { findRootNodeKey } from "@/konva_mindmap/utils/findRootNodeKey";
 
 export default function MindMapCanvas({ showMinutes, handleShowMinutes }) {
@@ -23,9 +26,10 @@ export default function MindMapCanvas({ showMinutes, handleShowMinutes }) {
     loading,
   } = useNodeListContext();
   const { dimensions, targetRef, handleWheel, zoomIn, zoomOut } = useDimension(data);
-  const registerLayer = useCollisionDetection(data, updateNode);
-  const stageRef = useRef();
   const { registerStageRef } = useStageStore();
+  const registerLayer = useCollisionDetection(data, updateNode);
+  const stageRef = useRef<Konva.Stage>(null);
+  const [isDragMode, setDragMode] = useState(false);
 
   const rootKey = findRootNodeKey(data);
 
@@ -45,7 +49,16 @@ export default function MindMapCanvas({ showMinutes, handleShowMinutes }) {
 
   useWindowKeyEventListener("keydown", (e) => {
     if (e.ctrlKey || e.metaKey) {
-      commandKeyMap[e.key]();
+      commandKeyMap[e.key]?.();
+    }
+    if (e.code === "Space") {
+      setDragMode(true);
+    }
+  });
+
+  useWindowKeyEventListener("keyup", (e) => {
+    if (e.code === "Space") {
+      setDragMode(false);
     }
   });
 
@@ -53,21 +66,30 @@ export default function MindMapCanvas({ showMinutes, handleShowMinutes }) {
     <div ref={targetRef} className="relative h-full min-h-0 w-full min-w-0 rounded-[20px] bg-white">
       <Stage
         ref={stageRef}
-        className="cursor-pointer"
         width={dimensions.width}
         height={dimensions.height}
         scaleX={dimensions.scale}
         scaleY={dimensions.scale}
         x={dimensions.x}
         y={dimensions.y}
-        draggable
         onWheel={handleWheel}
+        draggable={isDragMode}
+        className={`${isDragMode ? "cursor-pointer" : ""}`}
       >
         <Layer ref={registerLayer}>
-          {Object.keys(data).length >= 1 && <DrawMindMap data={data} root={data[rootKey]} depth={1} />}
+          {Object.keys(data).length >= 1 && (
+            <MindMapNode data={data} node={data[rootKey]} depth={1} dragmode={isDragMode} />
+          )}
+          <SelectionRect stage={stageRef} dragmode={isDragMode} />
         </Layer>
       </Stage>
-      <ToolMenu dimensions={dimensions} zoomIn={zoomIn} zoomOut={zoomOut} />
+      <ToolMenu
+        dimensions={dimensions}
+        zoomIn={zoomIn}
+        zoomOut={zoomOut}
+        dragmode={isDragMode}
+        setDragmode={setDragMode}
+      />
       {!Object.keys(data).length && !loading ? (
         <NoNodeInform />
       ) : (
