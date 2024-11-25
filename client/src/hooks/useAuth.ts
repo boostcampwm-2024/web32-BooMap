503fee6a5e562d54f26210fe271d96090ac128f8
Minhyung Cho <dup33333@hufs.ac.kr>
2024-11-25 21:31:19 +0900

    fix : 로그인 에러처리

--- client/src/hooks/useAuth.ts
+++ client/src/hooks/useAuth.ts
@@ -3,18 +3,22 @@ import { useAuthStore } from "@/store/useAuthStore";
 import { getToken, removeToken } from "@/utils/token";
 import { useQuery } from "@tanstack/react-query";
 import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function useAuth() {
   const accessToken = getToken();
   const auth = useAuthStore();
+  const navigate = useNavigate();
+
   const { isLoading, isError } = useQuery({
     queryKey: ["user"],
     queryFn: getUser,
     enabled: !!(accessToken || auth.isAuthenticated),
   });
+
   useEffect(() => {
     if (isError) {
-      removeToken();
+      navigate("/error");
     }
   }, [isError]);
   return { isLoading };
