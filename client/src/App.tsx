6f424c92a6a9cba6c2094f02d0ab0bd226c8c099
Minhyung Cho <dup33333@hufs.ac.kr>
2024-11-25 18:01:32 +0900

    remove : 메인페이지 Error boundary 삭제

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,15 +2,11 @@ import MainPage from "@/pages/Main";
 import "./App.css";
 import { Suspense } from "react";
 import Spinner from "@/components/common/Spinner";
-import { ErrorBoundary } from "react-error-boundary";
-import NotFound from "@/components/common/NotFound";
 
 export default function App() {
   return (
-    <ErrorBoundary fallback={<NotFound />}>
-      <Suspense fallback={<Spinner />}>
-        <MainPage />
-      </Suspense>
-    </ErrorBoundary>
+    <Suspense fallback={<Spinner />}>
+      <MainPage />
+    </Suspense>
   );
 }
