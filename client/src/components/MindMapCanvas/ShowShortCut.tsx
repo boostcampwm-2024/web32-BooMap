4e36a6e13c0a6d745d7c449e886ad352e7a8b657
minju <alswn1583@naver.com>
2024-11-26 14:49:00 +0900

    refactor : ShowShortCut 컴포넌트 내부 확장성 좋게 수정

--- client/src/components/MindMapCanvas/ShowShortCut.tsx
+++ client/src/components/MindMapCanvas/ShowShortCut.tsx
@@ -1,7 +1,21 @@
+import React from "react";
 import { Button } from "@headlessui/react";
 import { FaLightbulb } from "react-icons/fa6";
 
+const shortcuts = [
+  { keys: ["Ctrl", "Z"], description: "뒤로가기" },
+  { keys: ["Ctrl", "Shift", "Z"], description: "앞으로가기" },
+  { keys: ["Ctrl", "Backspace"], description: "노드 삭제" },
+  { keys: ["Ctrl", "="], description: "노드 추가" },
+  { keys: ["Shift", "Drag"], description: "노드 다중 선택" },
+  { keys: ["SpaceBar"], description: "손모양 전환" },
+];
+
 export default function ShowShortCut() {
+  function Btn({ children }) {
+    return <span className="rounded-[6px] bg-white px-2 py-1 text-grayscale-500">{children}</span>;
+  }
+
   return (
     <div className="group relative bottom-[66px] left-[5px] text-sm">
       <Button className="p-2">
@@ -9,30 +23,20 @@ export default function ShowShortCut() {
       </Button>
       <div className="absolute bottom-[40px] left-2 mt-2 hidden rounded-md bg-grayscale-700 px-2 py-3 text-white group-hover:block">
         <div className="flex flex-col gap-3">
-          <p>
-            <Btn>Ctrl</Btn> + <Btn>Z</Btn> : 뒤로가기
-          </p>
-          <p>
-            <Btn>Ctrl</Btn> + <Btn>Shift</Btn> + <Btn>Z</Btn> : 앞으로가기
-          </p>
-          <p>
-            <Btn>Click</Btn> + <Btn>Backspace</Btn> : 노드 삭제
-          </p>
-          <p>
-            <Btn>Click</Btn> + <Btn>=</Btn> : 노드 추가
-          </p>
-          <p>
-            <Btn>Shift</Btn> + <Btn>Drag</Btn> : 노드 다중 선택
-          </p>
-          <p>
-            <Btn>Spacebar</Btn> : 손모양 전환
-          </p>
+          {shortcuts.map(({ keys, description }, index) => (
+            <p key={index}>
+              {keys.map((key, i) => (
+                <React.Fragment key={i}>
+                  <Btn>{key}</Btn>
+                  {i < keys.length - 1 && " + "}
+                </React.Fragment>
+              ))}
+              : {description}
+            </p>
+          ))}
         </div>
+        ;
       </div>
     </div>
   );
-}
-
-function Btn({ children }) {
-  return <span className="rounded-[6px] bg-white px-2 py-1 text-grayscale-500">{children}</span>;
 }
