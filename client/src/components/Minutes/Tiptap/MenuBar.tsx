df97bc4e0ed1e1c6b52a7361788c5b665e3596bb
minju <alswn1583@naver.com>
2024-11-26 14:49:52 +0900

    refactor : MenuBar 반복되는 부분 결합

--- client/src/components/Minutes/Tiptap/MenuBar.tsx
+++ client/src/components/Minutes/Tiptap/MenuBar.tsx
@@ -1,84 +1,101 @@
-import { BubbleMenu } from "@tiptap/react";
-import { BsTypeH1 } from "react-icons/bs";
-import { BsTypeH2 } from "react-icons/bs";
-import { BsTypeH3 } from "react-icons/bs";
-import { BsTypeBold } from "react-icons/bs";
-import { BsTypeStrikethrough } from "react-icons/bs";
+import { BubbleMenu, isActive, Editor } from "@tiptap/react";
+import {
+  BsTypeH1,
+  BsTypeH2,
+  BsTypeH3,
+  BsTypeBold,
+  BsTypeStrikethrough,
+  BsTextLeft,
+  BsTextCenter,
+  BsTextRight,
+  BsJustify,
+} from "react-icons/bs";
 import { PiHighlighter } from "react-icons/pi";
-import { BsTextLeft } from "react-icons/bs";
-import { BsTextCenter } from "react-icons/bs";
-import { BsTextRight } from "react-icons/bs";
-import { BsJustify } from "react-icons/bs";
+import { IconType } from "react-icons";
 
-export default function MenuBar({ editor }) {
+type MenuBarProps = {
+  editor: Editor | null;
+};
+
+type MenuButtonProps = {
+  action: () => void;
+  isActive: () => boolean;
+  Icon: IconType;
+};
+
+export default function MenuBar({ editor }: MenuBarProps) {
   if (!editor) {
     return null;
+  }
+
+  const menuButtons = [
+    {
+      action: () => editor.chain().focus().toggleHeading({ level: 1 }).run(),
+      isActive: () => editor.isActive("heading", { level: 1 }),
+      Icon: BsTypeH1,
+    },
+    {
+      action: () => editor.chain().focus().toggleHeading({ level: 2 }).run(),
+      isActive: () => editor.isActive("heading", { level: 2 }),
+      Icon: BsTypeH2,
+    },
+    {
+      action: () => editor.chain().focus().toggleHeading({ level: 3 }).run(),
+      isActive: () => editor.isActive("heading", { level: 3 }),
+      Icon: BsTypeH3,
+    },
+    {
+      action: () => editor.chain().focus().toggleBold().run(),
+      isActive: () => editor.isActive("bold"),
+      Icon: BsTypeBold,
+    },
+    {
+      action: () => editor.chain().focus().toggleStrike().run(),
+      isActive: () => editor.isActive("strike"),
+      Icon: BsTypeStrikethrough,
+    },
+    {
+      action: () => editor.chain().focus().toggleHighlight().run(),
+      isActive: () => editor.isActive("highlight"),
+      Icon: PiHighlighter,
+    },
+    {
+      action: () => editor.chain().focus().setTextAlign("left").run(),
+      isActive: () => editor.isActive({ textAlign: "left" }),
+      Icon: BsTextLeft,
+    },
+    {
+      action: () => editor.chain().focus().setTextAlign("center").run(),
+      isActive: () => editor.isActive({ textAlign: "center" }),
+      Icon: BsTextCenter,
+    },
+    {
+      action: () => editor.chain().focus().setTextAlign("right").run(),
+      isActive: () => editor.isActive({ textAlign: "right" }),
+      Icon: BsTextRight,
+    },
+    {
+      action: () => editor.chain().focus().setTextAlign("justify").run(),
+      isActive: () => editor.isActive({ textAlign: "justify" }),
+      Icon: BsJustify,
+    },
+  ];
+
+  function MenuButton({ action, isActive, Icon }: MenuButtonProps) {
+    return (
+      <button onClick={action} className={isActive() ? "is-active" : ""}>
+        <Icon className="buttons" />
+      </button>
+    );
   }
 
   return (
     <>
       {editor && (
         <BubbleMenu className="bubble-menu" tippyOptions={{ duration: 100 }} editor={editor}>
-          <button
-            onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
-            className={editor.isActive("heading", { level: 1 }) ? "is-active" : ""}
-          >
-            <BsTypeH1 className="buttons" />
-          </button>
-          <button
-            onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
-            className={editor.isActive("heading", { level: 2 }) ? "is-active" : ""}
-          >
-            <BsTypeH2 className="buttons" />
-          </button>
-          <button
-            onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
-            className={editor.isActive("heading", { level: 3 }) ? "is-active" : ""}
-          >
-            <BsTypeH3 className="buttons" />
-          </button>
-          <button
-            onClick={() => editor.chain().focus().toggleBold().run()}
-            className={editor.isActive("bold") ? "is-active" : ""}
-          >
-            <BsTypeBold className="buttons" />
-          </button>
-          <button
-            onClick={() => editor.chain().focus().toggleStrike().run()}
-            className={editor.isActive("strike") ? "is-active" : ""}
-          >
-            <BsTypeStrikethrough className="buttons" />
-          </button>
-          <button
-            onClick={() => editor.chain().focus().toggleHighlight().run()}
-            className={editor.isActive("highlight") ? "is-active" : ""}
-          >
-            <PiHighlighter className="buttons" />
-          </button>
-          <button
-            onClick={() => editor.chain().focus().setTextAlign("left").run()}
-            className={editor.isActive({ textAlign: "left" }) ? "is-active" : ""}
-          >
-            <BsTextLeft className="buttons" />
-          </button>
-          <button
-            onClick={() => editor.chain().focus().setTextAlign("center").run()}
-            className={editor.isActive({ textAlign: "center" }) ? "is-active" : ""}
-          >
-            <BsTextCenter className="buttons" />
-          </button>
-          <button
-            onClick={() => editor.chain().focus().setTextAlign("right").run()}
-            className={editor.isActive({ textAlign: "right" }) ? "is-active" : ""}
-          >
-            <BsTextRight className="buttons" />
-          </button>
-          <button
-            onClick={() => editor.chain().focus().setTextAlign("justify").run()}
-            className={editor.isActive({ textAlign: "justify" }) ? "is-active" : ""}
-          >
-            <BsJustify className="buttons" />
-          </button>
+          {menuButtons.map((button, index) => (
+            <MenuButton key={index} action={button.action} isActive={button.isActive} Icon={button.Icon} />
+          ))}
         </BubbleMenu>
       )}
     </>
