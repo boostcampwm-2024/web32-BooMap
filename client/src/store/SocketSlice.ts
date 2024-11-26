e96f6796a95eefe18d750c0d7ee83e54c160735b
kimnamheeee <nami200284@snu.ac.kr>
2024-11-26 16:31:05 +0900

    refactor : handleConnection을 socketslice 내부로 이동

--- client/src/store/SocketSlice.ts
+++ client/src/store/SocketSlice.ts
@@ -1,12 +1,15 @@
 import { Socket, io } from "socket.io-client";
 import { create } from "zustand";
 import { actionType, createNodePayload, updateNodePayload, deleteNodePayload } from "@/types/NodePayload";
+import { createMindmap } from "@/api/mindmap.api";
+import { NavigateFunction } from "react-router-dom";
 
 type SocketState = {
   socket: Socket | null;
   connectSocket: (id: string) => void;
   disconnectSocket: () => void;
   handleSocketEvent: (props: HandleSocketEventProps) => void;
+  handleConnection: (navigate: NavigateFunction, targetMode: string) => void;
 };
 
 type HandleSocketEventProps = {
@@ -45,5 +48,18 @@ export const SocketSlice = create<SocketState>((set, get) => ({
     socket.on(actionType, (response) => {
       if (response && callback) callback(response);
     });
+  },
+
+  handleConnection: async (navigate: NavigateFunction, targetMode: string) => {
+    try {
+      const socket = get().socket;
+      if (socket) socket.disconnect();
+      const response = await createMindmap();
+      const newMindMapId = response.data;
+      get().connectSocket(newMindMapId);
+      navigate(`/mindmap/${newMindMapId}?mode=${targetMode}`);
+    } catch (error) {
+      console.error(error);
+    }
   },
 }));
