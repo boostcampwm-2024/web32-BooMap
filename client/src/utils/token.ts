ef4558284f828fd1c82ee765b63d1ea6a4952617
Minhyung Cho <dup33333@hufs.ac.kr>
2024-11-27 21:38:03 +0900

    feat : 비로그인 유저 localstorage로 소유자 구분하기 위한 유틸 함수 추가

--- client/src/utils/token.ts
+++
@@ -1,3 +0,0 @@
-export const getToken = () => localStorage.getItem("Bearer");
-export const removeToken = () => localStorage.removeItem("Bearer");
-export const setToken = (token: string) => localStorage.setItem("Bearer", token);
