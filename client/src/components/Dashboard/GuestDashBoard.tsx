22fb58783b86f6e72652ad9120498a81425cba80
Minhyung Cho <dup33333@hufs.ac.kr>
2024-11-27 15:17:12 +0900

    feat : 비회원 상태 마인드맵 생성 확인 모달 추가

--- client/src/components/Dashboard/GuestDashBoard.tsx
+++ client/src/components/Dashboard/GuestDashBoard.tsx
@@ -1,17 +1,14 @@
 import dashboardIcon from "@/assets/dashbordIcon.png";
 import plusIcon from "@/assets/plus.png";
+import GuestNewMindMapModal from "@/components/Dashboard/GuestNewMindMapModal";
 import LoginModal from "@/components/LoginModal";
-import { useSocketStore } from "@/store/useSocketStore";
 import { Button } from "@headlessui/react";
 import { useState } from "react";
 import { createPortal } from "react-dom";
-import { useNavigate } from "react-router-dom";
 
 export default function GuestDashBoard() {
   const [loginModal, setLoginModal] = useState(false);
-
-  const navigate = useNavigate();
-  const { handleConnection } = useSocketStore();
+  const [confirmModal, setConfirmModal] = useState(false);
 
   return (
     <>
@@ -24,11 +21,19 @@ export default function GuestDashBoard() {
         <img className="w-[300px]" src={dashboardIcon} alt="대쉬보드 아이콘" />
         <Button
           className="group flex w-[300px] items-center justify-center gap-3 rounded-[10px] bg-grayscale-600 px-10 py-2 text-grayscale-200 hover:text-white"
-          onClick={() => handleConnection(navigate, "textupload")}
+          onClick={() => setConfirmModal(true)}
         >
           <img className="w-6 group-hover:brightness-0 group-hover:invert" src={plusIcon} alt="추가하기 아이콘" />
           <p className="text-xl text-grayscale-200 group-hover:text-white">새로운 마인드맵 만들기</p>
         </Button>
+        <GuestNewMindMapModal
+          open={confirmModal}
+          closeModal={() => setConfirmModal(false)}
+          openLogin={() => {
+            setLoginModal(true);
+            setConfirmModal(false);
+          }}
+        />
         <Button
           className="mt-4 w-[300px] rounded-[10px] bg-grayscale-600 px-24 py-2 text-xl text-grayscale-200 hover:text-white"
           onClick={() => setLoginModal(true)}
