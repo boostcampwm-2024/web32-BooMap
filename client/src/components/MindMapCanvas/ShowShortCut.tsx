e1530823ef892ee8c4d9c77f15c3a332105404b4
Minju9187 <alswn1583@naver.com>
2024-11-27 18:01:58 +0900

    refactor : 단축키 내용 수정 & 위치 수정

--- client/src/components/MindMapCanvas/ShowShortCut.tsx
+++ client/src/components/MindMapCanvas/ShowShortCut.tsx
@@ -3,12 +3,12 @@ import { Button } from "@headlessui/react";
 import { FaLightbulb } from "react-icons/fa6";
 
 const shortcuts = [
-  { keys: ["Ctrl", "Z"], description: "뒤로가기" },
-  { keys: ["Ctrl", "Shift", "Z"], description: "앞으로가기" },
-  { keys: ["Ctrl", "Backspace"], description: "노드 삭제" },
-  { keys: ["Ctrl", "="], description: "노드 추가" },
+  { keys: ["="], description: "노드 추가" },
+  { keys: ["Backspace"], description: "노드 삭제" },
   { keys: ["Shift", "Drag"], description: "노드 다중 선택" },
   { keys: ["SpaceBar"], description: "손모양 전환" },
+  { keys: ["Ctrl", "Z"], description: "뒤로가기" },
+  { keys: ["Ctrl", "Shift", "Z"], description: "앞으로가기" },
 ];
 
 export default function ShowShortCut() {
@@ -17,11 +17,11 @@ export default function ShowShortCut() {
   }
 
   return (
-    <div className="group relative bottom-[66px] left-[5px] text-sm">
-      <Button className="p-2">
+    <div className="group absolute left-2 top-2">
+      <div className="cursor-pointer p-2">
         <FaLightbulb size={20} color="#98A4EE" />
-      </Button>
-      <div className="absolute bottom-[40px] left-2 mt-2 hidden rounded-md bg-grayscale-700 px-2 py-3 text-white group-hover:block">
+      </div>
+      <div className="absolute left-2 top-[35px] hidden w-64 rounded-lg bg-grayscale-700 px-2 py-3 text-white group-hover:block">
         <div className="flex flex-col gap-3">
           {shortcuts.map(({ keys, description }, index) => (
             <p key={index}>
