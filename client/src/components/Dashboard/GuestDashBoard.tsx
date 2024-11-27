89daff717217bf408b4ac383d1942ef0320e1c14
Minhyung Cho <dup33333@hufs.ac.kr>
2024-11-27 21:25:17 +0900

    refactor : useModal 사용하여 모달 제어 방식으로 변경

--- client/src/components/Dashboard/GuestDashBoard.tsx
+++ client/src/components/Dashboard/GuestDashBoard.tsx
@@ -2,13 +2,12 @@ import dashboardIcon from "@/assets/dashbordIcon.png";
 import plusIcon from "@/assets/plus.png";
 import GuestNewMindMapModal from "@/components/Dashboard/GuestNewMindMapModal";
 import LoginModal from "@/components/LoginModal";
+import useModal from "@/hooks/useModal";
 import { Button } from "@headlessui/react";
-import { useState } from "react";
-import { createPortal } from "react-dom";
 
 export default function GuestDashBoard() {
-  const [loginModal, setLoginModal] = useState(false);
-  const [confirmModal, setConfirmModal] = useState(false);
+  const { closeModal: closeLoginModal, open: loginModal, openModal: openLoginModal } = useModal();
+  const { closeModal: closeConfirmModal, open: confirmModal, openModal: openConfirmModal } = useModal();
 
   return (
     <>
@@ -21,27 +20,27 @@ export default function GuestDashBoard() {
         <img className="w-[300px]" src={dashboardIcon} alt="대쉬보드 아이콘" />
         <Button
           className="group flex w-[300px] items-center justify-center gap-3 rounded-[10px] bg-grayscale-600 px-10 py-2 text-grayscale-200 hover:text-white"
-          onClick={() => setConfirmModal(true)}
+          onClick={openConfirmModal}
         >
           <img className="w-6 group-hover:brightness-0 group-hover:invert" src={plusIcon} alt="추가하기 아이콘" />
           <p className="text-xl text-grayscale-200 group-hover:text-white">새로운 마인드맵 만들기</p>
         </Button>
         <GuestNewMindMapModal
           open={confirmModal}
-          closeModal={() => setConfirmModal(false)}
+          closeModal={closeConfirmModal}
           openLogin={() => {
-            setLoginModal(true);
-            setConfirmModal(false);
+            openLoginModal();
+            closeConfirmModal();
           }}
         />
         <Button
           className="mt-4 w-[300px] rounded-[10px] bg-grayscale-600 px-24 py-2 text-xl text-grayscale-200 hover:text-white"
-          onClick={() => setLoginModal(true)}
+          onClick={openLoginModal}
         >
           로그인하기
         </Button>
       </div>
-      {loginModal && createPortal(<LoginModal open={loginModal} close={() => setLoginModal(false)} />, document.body)}
+      <LoginModal open={loginModal} close={closeLoginModal} />
     </>
   );
 }
