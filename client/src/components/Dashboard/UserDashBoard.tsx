d2d72c5981b8c1e312ff5182e3daa5b00e368f13
Minhyung Cho <dup33333@hufs.ac.kr>
2024-11-28 00:50:02 +0900

    refactor : 쿼리 훅을 사용하여 데이터를 받아오고 선언형으로 코드 작성

--- client/src/components/Dashboard/UserDashBoard.tsx
+++ client/src/components/Dashboard/UserDashBoard.tsx
@@ -7,140 +7,24 @@ import { FaPlus } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 import { useSocketStore } from "@/store/useSocketStore";
 import { useAuthStore } from "@/store/useAuthStore";
-
-const apiData = {
-  mindMaps: [
-    {
-      id: 1,
-      connectionId: "asdf12-asd1f8-asd1f5-asdf",
-      title: "점심 메뉴를 골라볼까나~?",
-      keyword: ["점심메뉴", "한식", "중식", "양식", "일식", "피자", "초밥"],
-      createdDate: new Date(),
-      modifiedDate: new Date(),
-      owner: "강민주",
-    },
-    {
-      id: 2,
-      connectionId: "asdf12-asd1f8-asd1f5-asdf",
-      title: "이번 주 주말에 뭐할까?",
-      keyword: ["주말에 갈 곳", "놀이공원", "더현대 서울", "카페", "쇼핑", "놀이기구"],
-      createdDate: new Date(),
-      modifiedDate: new Date(),
-      owner: "양현호",
-    },
-    {
-      id: 3,
-      connectionId: "asdf12-asd1f8-asd1f5-asdf",
-      title: "여행",
-      keyword: ["여행", "국내", "국외", "부산", "제주도", "여수", "일본", "베트남", "미국"],
-      createdDate: new Date(),
-      modifiedDate: new Date(),
-      owner: "조민형",
-    },
-    {
-      id: 4,
-      connectionId: "asdf12-asd1f8-asd1f5-asdf",
-      title: "프로젝트 회의",
-      keyword: ["회의", "에러 발생!!", "추가 기능", "진행상황공유", "UI 에러..."],
-      createdDate: new Date(),
-      modifiedDate: new Date(),
-      owner: "김남희",
-    },
-    {
-      id: 5,
-      connectionId: "asdf12-asd1f8-asd1f5-asdf",
-      title: "점심 메뉴를 골라볼까나~?",
-      keyword: ["점심메뉴", "한식", "중식", "양식", "일식", "피자", "초밥"],
-      createdDate: new Date(),
-      modifiedDate: new Date(),
-      owner: "강민주",
-    },
-    {
-      id: 6,
-      connectionId: "asdf12-asd1f8-asd1f5-asdf",
-      title: "이번 주 주말에 뭐할까?",
-      keyword: ["주말에 갈 곳", "놀이공원", "더현대 서울", "카페", "쇼핑", "놀이기구"],
-      createdDate: new Date(),
-      modifiedDate: new Date(),
-      owner: "양현호",
-    },
-    {
-      id: 7,
-      connectionId: "asdf12-asd1f8-asd1f5-asdf",
-      title: "여행",
-      keyword: ["여행", "국내", "국외", "부산", "제주도", "여수", "일본", "베트남", "미국"],
-      createdDate: new Date(),
-      modifiedDate: new Date(),
-      owner: "조민형",
-    },
-    {
-      id: 8,
-      connectionId: "asdf12-asd1f8-asd1f5-asdf",
-      title: "프로젝트 회의",
-      keyword: ["회의", "에러 발생!!", "추가 기능", "진행상황공유", "UI 에러..."],
-      createdDate: new Date(),
-      modifiedDate: new Date(),
-      owner: "김남희",
-    },
-    {
-      id: 9,
-      connectionId: "asdf12-asd1f8-asd1f5-asdf",
-      title: "점심 메뉴를 골라볼까나~?",
-      keyword: ["점심메뉴", "한식", "중식", "양식", "일식", "피자", "초밥"],
-      createdDate: new Date(),
-      modifiedDate: new Date(),
-      owner: "강민주",
-    },
-    {
-      id: 10,
-      connectionId: "asdf12-asd1f8-asd1f5-asdf",
-      title: "이번 주 주말에 뭐할까?",
-      keyword: ["주말에 갈 곳", "놀이공원", "더현대 서울", "카페", "쇼핑", "놀이기구"],
-      createdDate: new Date(),
-      modifiedDate: new Date(),
-      owner: "양현호",
-    },
-    {
-      id: 11,
-      connectionId: "asdf12-asd1f8-asd1f5-asdf",
-      title: "여행",
-      keyword: ["여행", "국내", "국외", "부산", "제주도", "여수", "일본", "베트남", "미국"],
-      createdDate: new Date(),
-      modifiedDate: new Date(),
-      owner: "조민형",
-    },
-    {
-      id: 12,
-      connectionId: "asdf12-asd1f8-asd1f5-asdf",
-      title: "프로젝트 회의",
-      keyword: ["회의", "에러 발생!!", "추가 기능", "진행상황공유", "UI 에러..."],
-      createdDate: new Date(),
-      modifiedDate: new Date(),
-      owner: "김남희",
-    },
-  ],
-};
+import useDashBoard from "@/api/fetchHooks/useDashBoard";
+import NoMindMap from "@/components/Dashboard/NoMindMap";
 
 export default function UserDashBoard() {
-  const [data, setData] = useState(apiData.mindMaps);
+  const { data } = useDashBoard();
   const [searchContent, setSearchContent] = useState("");
   const navigate = useNavigate();
   const handleConnection = useSocketStore((state) => state.handleConnection);
   const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
 
-  function handleDeleteData(id: number) {
-    const newData = data.filter((item) => item.id !== id);
-    setData(newData);
-  }
-
   function searchData(content: string) {
-    const filteredData = data.filter(
-      (item) => item.title.includes(content) || item.keyword.some((k) => k.includes(content)),
-    );
-    return filteredData;
+    if (!data) return [];
+    return data.filter((item) => item.title.includes(content) || item.keyword.some((k) => k.includes(content)));
   }
 
   const filteredData = searchData(searchContent);
+
+  if (!data.length) return <NoMindMap />;
 
   return (
     <div className="relative flex h-full w-full flex-col rounded-[20px] bg-grayscale-700 px-8 pb-24 pt-8">
@@ -152,7 +36,7 @@ export default function UserDashBoard() {
       </header>
       <div className="no-scrollbar h-[calc(100%-40px)] overflow-y-scroll border-b-[1px] border-t-[1px] border-grayscale-500">
         {filteredData.map((info, i) => (
-          <MindMapInfoItem key={i} data={info} index={i} handleDeleteData={handleDeleteData} />
+          <MindMapInfoItem key={i} data={info} index={i} />
         ))}
       </div>
       <div className="absolute bottom-8 right-8">
@@ -172,7 +56,7 @@ export default function UserDashBoard() {
           }}
           placeholder="키워드나 제목을 입력하세요"
         />
-        <Button onClick={() => searchData(searchContent)}>
+        <Button>
           <img className="h-6 w-6" src={searchIcon} alt="검색하기 버튼" />
         </Button>
       </div>
