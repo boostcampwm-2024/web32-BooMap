1146b0fa1391c7a863f2b85ba999909253f0c9ac
Minju9187 <alswn1583@naver.com>
2024-11-28 15:50:56 +0900

    chore : text 크기 조정

--- client/src/konva_mindmap/utils/nodeAttrs.ts
+++ client/src/konva_mindmap/utils/nodeAttrs.ts
@@ -6,7 +6,7 @@ export const NODE_WIDTH_AND_HEIGHT = 100;
 export const NODE_RADIUS = (depth: number) => NODE_DEFAULT_SIZE - depth * NODE_DEFAULT_RATIO;
 export const TEXT_OFFSET_X = (depth: number) => NODE_DEFAULT_SIZE - depth * 10;
 export const TEXT_OFFSET_Y = (depth: number) => 5 * depth - NODE_DEFAULT_SIZE - 5;
-export const TEXT_WIDTH = (depth: number) => NODE_DEFAULT_SIZE * 2 - depth * 20;
+export const TEXT_WIDTH = (depth: number) => NODE_DEFAULT_SIZE * 2 - depth * 18;
 
 //CONNECTED_LINE
 export const CONNECTED_LINE_FROM = (depth: number) => NODE_DEFAULT_SIZE - depth * 7 + 10;
