766b2997d8766a4a1abbcc7bd39a88876f022950
Minhyung Cho <dup33333@hufs.ac.kr>
2024-11-28 20:53:12 +0900

    feat : 마인드맵 이동 전 redis로 옮겨주기 위한 get API 추가

--- client/src/api/mindmap.api.ts
+++ client/src/api/mindmap.api.ts
@@ -4,6 +4,10 @@ export function createMindmap() {
   return instance.post("/connection");
 }
 
+export function getMindMap(mindMapId) {
+  return instance.get(`/connection/${mindMapId}`);
+}
+
 export function deleteMindMap(mindMapId: string) {
   return instance.delete(`/mindmap/${mindMapId}`);
 }
