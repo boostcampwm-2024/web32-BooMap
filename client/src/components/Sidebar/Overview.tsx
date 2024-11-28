a4f45e4a00b82fdd5852ba66fe5c4ca358900664
Minhyung Cho <dup33333@hufs.ac.kr>
2024-11-28 16:01:41 +0900

    refactor : 아이콘 변경

--- client/src/components/Sidebar/Overview.tsx
+++ client/src/components/Sidebar/Overview.tsx
@@ -1,8 +1,4 @@
-import dashBoardIcon from "@/assets/dashboard.png";
-import listIcon from "@/assets/list.png";
 import aiIcon from "@/assets/ai.png";
-import voiceIcon from "@/assets/voiceFile.png";
-import textIcon from "@/assets/textFile.png";
 import OverviewButton from "@/components/Sidebar/OverviewButton";
 import useSection from "@/hooks/useSection";
 import { useNavigate } from "react-router-dom";
@@ -13,6 +9,11 @@ import useModal from "@/hooks/useModal";
 import { createPortal } from "react-dom";
 import LatestMindMapModal from "@/components/Sidebar/LatestMindMapModal";
 import { useState } from "react";
+import { RxDashboard } from "react-icons/rx";
+import { FaListUl } from "react-icons/fa";
+import { FaFileAlt } from "react-icons/fa";
+import { FaFileAudio } from "react-icons/fa6";
+import { RiRobot3Line } from "react-icons/ri";
 
 type ViewMode = "listview" | "voiceupload" | "textupload";
 export default function Overview() {
@@ -49,48 +50,44 @@ export default function Overview() {
     <div className="mt-14 flex flex-col text-base">
       <p className="mb-8 font-medium text-grayscale-500">OVERVIEW</p>
       <div className="flex flex-col gap-2 text-grayscale-400">
+        <OverviewButton text="대시보드" active={getmode() === "dashboard"} onclick={() => navigate("/")}>
+          <RxDashboard className="h-5 w-5" />
+        </OverviewButton>
         <OverviewButton
-          src={dashBoardIcon}
-          alt="대시보드"
-          text="대시보드"
-          active={getmode() === "dashboard"}
-          onclick={() => navigate("/")}
-        />
-        <OverviewButton
-          src={listIcon}
-          alt="리스트"
           text="리스트로 보기"
           active={getmode() === "listview"}
           onclick={() => {
             setSelcetMode("listview");
             navigateMindmap("listview");
           }}
-        />
-        <div className="flex items-center gap-6 rounded-lg p-3">
-          <img src={aiIcon} alt="ai변환" className="w-6" />
+        >
+          <FaListUl className="h-5 w-5" />
+        </OverviewButton>
+        <div className="ml-[2px] flex items-center gap-6 rounded-lg p-3">
+          <RiRobot3Line className="h-6 w-6" />
           AI 변환
         </div>
         <div className="flex flex-col gap-2 pl-5">
           <OverviewButton
-            src={voiceIcon}
-            alt="음성 파일"
             text="음성 파일 업로드"
             active={getmode() === "voiceupload"}
             onclick={() => {
               setSelcetMode("voiceupload");
               navigateMindmap("voiceupload");
             }}
-          />
+          >
+            <FaFileAudio className="h-5 w-5" />
+          </OverviewButton>
           <OverviewButton
-            src={textIcon}
-            alt="텍스트 파일"
             text="텍스트 파일 업로드"
             active={getmode() === "textupload"}
             onclick={() => {
               setSelcetMode("textupload");
               navigateMindmap("textupload");
             }}
-          />
+          >
+            <FaFileAlt className="h-5 w-5" />
+          </OverviewButton>
         </div>
       </div>
       {createPortal(
