1d082fb59353fc2f49bfe84f8683c047ac448cb2
Minhyung Cho <dup33333@hufs.ac.kr>
2024-11-29 07:07:19 +0900

    chore : 마인드맵 타입 추가

--- client/src/api/mindmap.api.ts
+++ client/src/api/mindmap.api.ts
@@ -1,10 +1,11 @@
 import { instance } from "@/api";
+import { MindMap } from "@/types/mindmap";
 
 export function createMindmap() {
   return instance.post("/connection");
 }
 
-export function getMindMap(mindMapId) {
+export function getMindMap(mindMapId: string): Promise<MindMap> {
   return instance.get(`/connection/${mindMapId}`);
 }
 
