c8b1cfba752e5306fabe86531c34eedb2519ecd9
adkm12 <adkm@gachon.ac.kr>
2024-11-28 15:50:21 +0900

    refactor : refresh 로직 수정
    - 소켓 서버에서 바로 access토큰 발급할 수 있도록 refresh토큰에 데이터 넣...었...습..닏..다

--- BE/apps/api-server/src/modules/auth/auth.service.ts
+++ BE/apps/api-server/src/modules/auth/auth.service.ts
@@ -1,50 +1,64 @@
 import { RedisService } from '@liaoliaots/nestjs-redis';
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, Logger } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { User } from '@app/entity';
 import Redis from 'ioredis';
 
-interface accessTokenPayload {
+interface tokenPayload {
   email: string;
-  id: number;
-}
-
-interface refreshTokenPayload {
   id: number;
 }
 
 @Injectable()
 export class AuthService {
-  private redis: Redis | null;
+  private GeneralRedis: Redis | null;
+  private readonly logger = new Logger(AuthService.name);
   constructor(
     private readonly jwtService: JwtService,
     private readonly redisService: RedisService,
   ) {
-    this.redis = this.redisService.getOrThrow();
+    this.GeneralRedis = this.redisService.getOrThrow('general');
   }
 
   generateAccessToken(user: User) {
-    const payload: accessTokenPayload = { email: user.email, id: user.id };
+    const payload = { email: user.email, id: user.id };
     const accessToken = this.jwtService.sign(payload, { expiresIn: 60 * 30 });
     return accessToken;
   }
 
   generateRefreshToken(user: User) {
-    const payload: refreshTokenPayload = { id: user.id };
+    const payload = { id: user.id, email: user.email };
     const refreshToken = this.jwtService.sign(payload, { expiresIn: '3d' });
     return refreshToken;
   }
 
   async verifiedRefreshToken(refreshToken: string) {
-    const isBlacklisted = await this.redis.get(refreshToken);
+    const isBlacklisted = await this.GeneralRedis.get(refreshToken);
     if (isBlacklisted === 'true') {
       throw new BadRequestException('다시 로그인해주세요.');
     }
-    const userId = this.jwtService.verify<refreshTokenPayload>(refreshToken).id;
-    return userId;
+
+    try {
+      this.jwtService.verify(refreshToken);
+      const decoded = this.jwtService.decode(refreshToken);
+      if (!decoded || typeof decoded !== 'object') {
+        throw new BadRequestException('Invalid token format');
+      }
+
+      const payload: tokenPayload = {
+        email: decoded.email,
+        id: decoded.id,
+      };
+
+      const accessToken = this.jwtService.sign(payload, { expiresIn: 60 * 30 });
+      return accessToken;
+    } catch (error) {
+      this.logger.error(error);
+      throw new BadRequestException('다시 로그인해  주세요.');
+    }
   }
 
   async logout(refreshToken: string) {
-    await this.redis.set(refreshToken, 'true', 'EX', 60 * 60 * 24 * 3);
+    await this.GeneralRedis.set(refreshToken, 'true', 'EX', 60 * 60 * 24 * 3);
   }
 }
