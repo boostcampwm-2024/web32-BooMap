647396bc3620084c46ee3ca8bda0bf0e2d341a96
Minhyung Cho <dup33333@hufs.ac.kr>
2024-11-30 21:09:36 +0900

    refactor : createConnection 인자 변경으로 인한 로직 수정

--- client/src/components/Dashboard/NoMindMap.tsx
+++ client/src/components/Dashboard/NoMindMap.tsx
@@ -5,9 +5,8 @@ import cloud from "@/assets/dashbordIcon.png";
 import { useConnectionStore } from "@/store/useConnectionStore";
 
 export default function NoMindMap() {
-  const handleConnection = useConnectionStore((state) => state.createConnection);
+  const createConnection = useConnectionStore((state) => state.createConnection);
   const navigate = useNavigate();
-  const isAuthenticated = useConnectionStore((state) => state.isAuthenticated);
   return (
     <div className="mb-20 flex h-full w-full flex-col items-center justify-center rounded-[20px] bg-grayscale-700 pb-10">
       <p className="whitespace-pre-line text-center text-3xl font-bold">
@@ -18,7 +17,7 @@ export default function NoMindMap() {
       <img className="w-[300px]" src={cloud} alt="로그인 후 마인드맵 없을 때 아이콘" />
       <Button
         className="group mt-4 flex w-[300px] items-center justify-center gap-3 rounded-[10px] bg-bm-blue px-10 py-2 transition hover:brightness-90"
-        onClick={() => handleConnection(navigate, "textupload", isAuthenticated)}
+        onClick={() => createConnection(navigate, "textupload")}
       >
         새로운 마인드맵 만들기
       </Button>
