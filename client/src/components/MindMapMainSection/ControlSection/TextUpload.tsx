53b01e4a6c579772e5b4506fb75374198ad32941
kimnamheeee <nami200284@snu.ac.kr>
2024-12-01 03:16:09 +0900

    design : clovaX 기술 지원 로고 추가

--- client/src/components/MindMapMainSection/ControlSection/TextUpload.tsx
+++ client/src/components/MindMapMainSection/ControlSection/TextUpload.tsx
@@ -1,12 +1,13 @@
 import { TEXT_UPLOAD_LIMIT } from "@/constants/textUploadLimit";
-import { Button, Input, Textarea } from "@headlessui/react";
+import { Button, Textarea } from "@headlessui/react";
 import { useState } from "react";
+import clovaX from "@/assets/clovaX.png";
 
 export default function TextUpload() {
   const [content, setContent] = useState("");
 
   return (
-    <div className="flex h-full flex-col gap-12 text-grayscale-100">
+    <div className="flex h-full flex-col gap-6 text-grayscale-100">
       <div className="flex h-full flex-col gap-4">
         <p>요약할 텍스트</p>
         <Textarea
@@ -14,12 +15,16 @@ export default function TextUpload() {
           placeholder="Text를 넣어주세요. (500자 이상 2000자 이하)"
           onKeyDown={(e) => e.stopPropagation()}
           onChange={(e) => setContent(e.target.value)}
+          maxLength={TEXT_UPLOAD_LIMIT}
         />
         <p className="text-right text-grayscale-400">
           {content.length}/{TEXT_UPLOAD_LIMIT}
         </p>
       </div>
       <Button className="rounded-xl bg-bm-blue p-3 transition hover:brightness-90">만들기</Button>
+      <div className="flex w-48 justify-end">
+        <img src={clovaX} alt="clovaX" />
+      </div>
     </div>
   );
 }
