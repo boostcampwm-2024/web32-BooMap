f6786ca8ed913618e1a9167ccc985f4b42d0a348
Minhyung Cho <dup33333@hufs.ac.kr>
2024-11-30 17:12:49 +0900

    feat : slice 패턴에 따른 합쳐지는 로직 추가

---
+++ client/src/store/useConnectionStore.ts
@@ -0,0 +1,36 @@
+import { createRoleSlice, RoleSlice } from "@/store/createRoleSlice";
+import { createSocketSlice, SocketSlice } from "@/store/createSocketSlice";
+import { createJSONStorage, devtools, persist } from "zustand/middleware";
+import { create } from "zustand";
+import { createMindMapOwnershipSlice, MindMapOwnershipSlice } from "@/store/createMindMapOwnershipSlice";
+import { ConnectionStore } from "@/types/store";
+import { createSharedSlice } from "@/store/createSharedSlice";
+import { createAuthSlice } from "@/store/createAuthSlice";
+
+type PersistStates = {
+  token: string;
+};
+
+export const useConnectionStore = create<ConnectionStore>()(
+  devtools(
+    persist(
+      (...a) => ({
+        ...createRoleSlice(...a),
+        ...createSocketSlice(...a),
+        ...createSharedSlice(...a),
+        ...createAuthSlice(...a),
+        ...createMindMapOwnershipSlice(...a),
+      }),
+      {
+        name: "connectionStore",
+        partialize: (state) => {
+          console.log("wwwwwww");
+          return {
+            token: state.token,
+          };
+        },
+        storage: createJSONStorage(() => localStorage),
+      },
+    ),
+  ),
+);
