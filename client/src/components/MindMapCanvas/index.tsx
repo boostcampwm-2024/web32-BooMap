0c0b17bc7924be69c7770def282723caaf69fcaa
kimnamheeee <nami200284@snu.ac.kr>
2024-11-30 18:26:26 +0900

    feat : 노드가 선택되어 있을 때 enter를 누르면 해당 노드의 input을 활성화

--- client/src/components/MindMapCanvas/index.tsx
+++ client/src/components/MindMapCanvas/index.tsx
@@ -1,5 +1,5 @@
 import useDimension from "@/konva_mindmap/hooks/useDimension";
-import useWindowKeyEventListener from "@/hooks/useWindowKeyEventListener";
+import useWindowEventListener from "@/hooks/useWindowKeyEventListener";
 import ToolMenu from "@/components/MindMapCanvas/ToolMenu";
 import initializeNodePosition from "@/konva_mindmap/utils/initializeNodePosition";
 import { Layer, Stage } from "react-konva";
@@ -55,7 +55,7 @@ export default function MindMapCanvas({ showMinutes, handleShowMinutes }) {
     });
   }
 
-  useWindowKeyEventListener("keydown", (e) => {
+  useWindowEventListener("keydown", (e) => {
     e.preventDefault();
     if (e.metaKey || e.ctrlKey) {
       if (e.shiftKey && e.code) redo();
@@ -91,7 +91,7 @@ export default function MindMapCanvas({ showMinutes, handleShowMinutes }) {
     }
   });
 
-  useWindowKeyEventListener("keyup", (e) => {
+  useWindowEventListener("keyup", (e) => {
     if (e.code === "Space") {
       setDragMode(false);
     }
