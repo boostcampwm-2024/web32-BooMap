b083a00ecb7a3820aaade4fb50eb475fb5731e44
Minhyung Cho <dup33333@hufs.ac.kr>
2024-11-30 21:07:50 +0900

    refactor : 회원-소유권 확인 로직 변경

--- client/src/store/createMindMapOwnershipSlice.ts
+++ client/src/store/createMindMapOwnershipSlice.ts
@@ -1,12 +1,13 @@
+import { DashBoard } from "@/konva_mindmap/types/dashboard";
 import { ConnectionStore } from "@/types/store";
 import { StateCreator } from "zustand";
 
 export interface MindMapOwnershipSlice {
   ownedMindMap: string[];
-  ownedMindForGuest: string[];
+  ownedMindMapForGuest: string[];
   addOwnedMindMap: (connectionId: string) => void;
   addOwnedMindMapForGuest: (connectionId: string) => void;
-  updateOwnedMindMap: (mindMaps: string[] | []) => void;
+  updateOwnedMindMap: (mindMaps: DashBoard[]) => void;
   deleteOwnedMindMap: (connectionId: string) => void;
   deleteOwnedMindMapForGuest: (connectionId: string) => void;
   resetOwnedMindMap: () => void;
@@ -17,13 +18,17 @@ export const createMindMapOwnershipSlice: StateCreator<ConnectionStore, [], [], 
   get,
 ) => ({
   ownedMindMap: [],
-  ownedMindForGuest: [],
+  ownedMindMapForGuest: [],
 
   addOwnedMindMap: (connectionId: string) => set((state) => ({ ownedMindMap: [...state.ownedMindMap, connectionId] })),
   addOwnedMindMapForGuest: (connectionId: string) =>
-    set((state) => ({ ownedMindMap: [...state.ownedMindMap, connectionId] })),
+    set((state) => ({ ownedMindMapForGuest: [...state.ownedMindMapForGuest, connectionId] })),
 
-  updateOwnedMindMap: (mindMaps: string[] | []) => set({ ownedMindMap: mindMaps }),
+  updateOwnedMindMap: (mindMaps: DashBoard[]) => {
+    const userId = get().id;
+    const userOwnedMaps = mindMaps.filter((map) => map.ownerId === userId).map((map) => map.connectionId);
+    set({ ownedMindMap: userOwnedMaps });
+  },
 
   deleteOwnedMindMap: (connectionId: string) =>
     set((state) => ({ ownedMindMap: state.ownedMindMap.filter((map) => map !== connectionId) })),
