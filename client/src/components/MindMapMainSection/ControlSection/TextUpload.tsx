124546d7d99091951ba2509bfbc0e9933beb8d7c
Minhyung Cho <dup33333@hufs.ac.kr>
2024-12-01 15:58:41 +0900

    feat : textupload 컴포넌트의 비즈니스 로직을 커스텀훅으로 분리 및 소유권자가 아닐 경우 안내 말풍선 추가

--- client/src/components/MindMapMainSection/ControlSection/TextUpload.tsx
+++ client/src/components/MindMapMainSection/ControlSection/TextUpload.tsx
@@ -1,10 +1,22 @@
-import { TEXT_UPLOAD_LIMIT } from "@/constants/textUploadLimit";
+import ArrowBox from "@/components/common/ArrowBox";
+import Spinner from "@/components/common/Spinner";
+import { MAX_TEXT_UPLOAD_LIMIT } from "@/constants/textUploadLimit";
+import useUpload from "@/hooks/useUpload";
 import { Button, Textarea } from "@headlessui/react";
-import { useState } from "react";
+import { createPortal } from "react-dom";
 import clovaX from "@/assets/clovaX.png";
 
 export default function TextUpload() {
-  const [content, setContent] = useState("");
+  const {
+    content,
+    aiProcessing,
+    buttonAvailability,
+    updateContent,
+    handleAiProcessButton,
+    availabilityInformBox,
+    handleMouseEnter,
+    handleMouseLeave,
+  } = useUpload();
 
   return (
     <div className="flex h-full flex-col gap-6 text-grayscale-100">
@@ -18,10 +30,27 @@ export default function TextUpload() {
           maxLength={TEXT_UPLOAD_LIMIT}
         />
         <p className="text-right text-grayscale-400">
-          {content.length}/{TEXT_UPLOAD_LIMIT}
+          {content.length}/{MAX_TEXT_UPLOAD_LIMIT}
         </p>
       </div>
-      <Button className="rounded-xl bg-bm-blue p-3 transition hover:brightness-90">만들기</Button>
+      <Button
+        className="relative rounded-xl bg-bm-blue p-3 transition hover:brightness-90"
+        onClick={handleAiProcessButton}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+        만들기
+        {availabilityInformBox && (
+          <ArrowBox
+            containerClassName="-left-0.5 -top-10 w-full text-sm"
+            boxClassName="bg-grayscale-500 p-1"
+            arrowClassName="bottom-0 left-1/2 z-0 h-3 w-3 -translate-x-1/2 translate-y-1/2 rotate-45 bg-grayscale-500"
+          >
+            <p>마인드맵을 소유한 사람만 ai 변환을 사용할 수 있어요</p>
+          </ArrowBox>
+        )}
+      </Button>
+      {aiProcessing && createPortal(<Spinner />, document.body)}
       <div className="flex w-48 justify-end">
         <img src={clovaX} alt="clovaX" />
       </div>
