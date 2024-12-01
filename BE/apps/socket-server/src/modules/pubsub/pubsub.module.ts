7ff884b68b802713da06a4af03817ee3c3709128
adkm12 <adkm@gachon.ac.kr>
2024-12-01 22:07:41 +0900

    refactor :  pubsub 모듈 제거, 소켓서버 로거 미들웨어 제거

--- BE/apps/socket-server/src/modules/pubsub/pubsub.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PublisherService } from './publisher.service';
-import { SubscriberService } from './subscriber.service';
-
-@Module({
-  providers: [PublisherService, SubscriberService],
-  exports: [PublisherService, SubscriberService],
-})
-export class PubsubModule {}
