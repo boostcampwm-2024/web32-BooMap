a841cd3a9133592eb23514d47c72b1d82be40585
kimnamheeee <nami200284@snu.ac.kr>
2024-12-02 02:49:33 +0900

    refactor : notFound -> error 컴포넌트 이름 변경
    notfound 페이지가 따로 생길 예정이라 변경합니다

--- client/src/components/common/Error.tsx
+++ client/src/components/common/Error.tsx
@@ -1,9 +1,9 @@
-import notFoundIcon from "@/assets/notFound";
+import errorIcon from "@/assets/error";
 import { Button } from "@headlessui/react";
-export default function NotFound() {
+export default function Error() {
   return (
     <div className="z-50 flex h-full w-full items-center justify-center gap-6">
-      <img className="w-96" src={notFoundIcon} alt="notfound" />
+      <img className="w-96" src={errorIcon} alt="error" />
       <div className="flex-center flex flex-col items-center">
         <p className="text-[100px] text-white">OOPS!</p>
         <p className="text-2xl text-grayscale-300">무언가 잘못됐어요</p>
