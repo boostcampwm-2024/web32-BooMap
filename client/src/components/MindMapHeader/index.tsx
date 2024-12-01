056e6b03d4abeec8cab84062f7bf38e46e02bac3
Minju9187 <alswn1583@naver.com>
2024-12-01 19:15:13 +0900

    refactor : 제목 더블클릭시 autofocus & 디자인 수정

--- client/src/components/MindMapHeader/index.tsx
+++ client/src/components/MindMapHeader/index.tsx
@@ -18,7 +18,12 @@ export default function MindMapHeader() {
   const role = useConnectionStore((state) => state.currentRole);
 
   function handleInputBlur() {
-    if (!title.length) return;
+    if (!editTitle.length) {
+      setEditTitle(title);
+      setEditMode(false);
+      return;
+    }
+
     handleSocketEvent({
       actionType: "updateTitle",
       payload: { title: editTitle },
@@ -43,12 +48,13 @@ export default function MindMapHeader() {
       <MindMapHeaderButtons />
       {editMode ? (
         <Input
-          className="flex w-80 items-center bg-transparent text-center"
+          className="flex w-80 items-center bg-transparent text-center focus:border-none focus:outline-none"
           value={editTitle}
           onChange={(e) => setEditTitle(e.target.value)}
           onBlur={handleInputBlur}
           onKeyDown={handleInputKeyDown}
           maxLength={32}
+          autoFocus
         />
       ) : (
         <span onDoubleClick={changeToEditMode} className="flex cursor-pointer items-center gap-3 text-lg">
