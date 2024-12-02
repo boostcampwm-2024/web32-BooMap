736970f7f090a9bb0438be9da453ebed9f4fba7a
adkm12 <adkm@gachon.ac.kr>
2024-12-03 01:21:30 +0900

    fix : HttpException 수정

--- BE/apps/api-server/src/modules/connection/connection.service.ts
+++ BE/apps/api-server/src/modules/connection/connection.service.ts
@@ -1,10 +1,9 @@
 import { MindmapService } from './../mindmap/mindmap.service';
 import { UserService } from './../user/user.service';
-import { Injectable, Logger, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import { ForbiddenException, Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { Redis } from 'ioredis';
 import { RedisService } from '@liaoliaots/nestjs-redis';
 import { v4 as uuidv4 } from 'uuid';
-import { ConnectionException } from '../../exceptions';
 
 @Injectable()
 export class ConnectionService {
@@ -39,37 +38,28 @@ export class ConnectionService {
   }
 
   async setConnection(mindmapId: number, userId: number) {
-    try {
-      const role = await this.userService.getRole(userId, mindmapId);
-      if (!role) {
-        throw new UnauthorizedException('권한이 없습니다.');
-      }
-
-      const owner = await this.mindmapService.getOwner(mindmapId);
-      const mindmapData = await this.mindmapService.getDataByMindmapId(mindmapId);
-      await this.GeneralRedis.hset(mindmapData.connectionId, {
-        type: 'user',
-        mindmapId: mindmapId,
-        aiCount: mindmapData.aiCount,
-        title: mindmapData.title,
-        ownerId: owner.pop().userId,
-      });
-
-      await this.GeneralRedis.set(`mindmapState:${mindmapData.connectionId}`, JSON.stringify(mindmapData.nodes));
-      await this.GeneralRedis.set(`content:${mindmapData.connectionId}`, mindmapData.content);
-
-      return {
-        mindmapId,
-        connectionId: mindmapData.connectionId,
-        role: role,
-      };
-    } catch (error) {
-      this.logger.error(error);
-      if (error instanceof UnauthorizedException) {
-        throw error;
-      } else {
-        throw new ConnectionException(error.message);
-      }
+    const role = await this.userService.getRole(userId, mindmapId);
+    if (!role) {
+      throw new ForbiddenException('권한이 없습니다.');
     }
+
+    const owner = await this.mindmapService.getOwner(mindmapId);
+    const mindmapData = await this.mindmapService.getDataByMindmapId(mindmapId);
+    await this.GeneralRedis.hset(mindmapData.connectionId, {
+      type: 'user',
+      mindmapId: mindmapId,
+      aiCount: mindmapData.aiCount,
+      title: mindmapData.title,
+      ownerId: owner.pop().userId,
+    });
+
+    await this.GeneralRedis.set(`mindmapState:${mindmapData.connectionId}`, JSON.stringify(mindmapData.nodes));
+    await this.GeneralRedis.set(`content:${mindmapData.connectionId}`, mindmapData.content);
+
+    return {
+      mindmapId,
+      connectionId: mindmapData.connectionId,
+      role: role,
+    };
   }
 }
--- BE/apps/api-server/src/modules/mindmap/mindmap.service.ts
+++ BE/apps/api-server/src/modules/mindmap/mindmap.service.ts
@@ -1,5 +1,5 @@
 import { InjectRepository } from '@nestjs/typeorm';
-import { Injectable, Logger, UnauthorizedException } from '@nestjs/common';
+import { ForbiddenException, Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { In, Repository } from 'typeorm';
 import { Mindmap, UserMindmapRole } from '@app/entity';
 import { v4 as uuidv4 } from 'uuid';
@@ -80,24 +80,27 @@ export class MindmapService {
     });
 
     if (role.role !== Role.OWNER) {
-      throw new UnauthorizedException('권한이 없습니다.');
+      throw new ForbiddenException('권한이 없습니다.');
     }
 
     const mindmap = await this.mindmapRepository.findOne({
       where: { id: mindmapId },
       relations: ['nodes', 'userMindmapRoles'],
     });
+
     if (!mindmap) {
-      throw new MindmapException('마인드맵을 찾을 수 없습니다.');
+      throw new NotFoundException('마인드맵을 찾을 수 없습니다.');
     }
+
     await this.mindmapRepository.softRemove(mindmap);
   }
 
   async getDataByMindmapId(mindmapId: number) {
     const mindmap = await this.mindmapRepository.findOne({ where: { id: mindmapId } });
     const nodes = await this.nodeService.tableToCanvas(mindmap.id);
+
     if (!mindmap) {
-      throw new MindmapException('마인드맵을 찾을 수 없습니다.');
+      throw new NotFoundException('마인드맵을 찾을 수 없습니다.');
     }
 
     return {
