1dff3777d15685247bd15a80e11cad3a3c1cf6d3
Minhyung Cho <dup33333@hufs.ac.kr>
2024-12-03 13:58:23 +0900

    refactor : ai 요청 전용 인스턴스 및 인터셉터 설정

--- client/src/api/ai.ts
+++ client/src/api/ai.ts
@@ -1,15 +1,30 @@
-import { instance } from "@/api";
+import { useConnectionStore } from "@/store/useConnectionStore";
+import { logOnDev } from "@/utils/logging";
+import axios from "axios";
+import { InternalAxiosRequestConfig } from "axios";
 
-export function AudioAiConvert(formData: FormData, mindmapId: string) {
-  return instance.post(
-    `ai/audio/${mindmapId}`,
-    {
-      formData,
+export const instanceForAi = axios.create({
+  baseURL: import.meta.env.VITE_APP_API_SERVER_BASE_URL,
+  timeout: 3000,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "multipart/form-data",
+  },
+});
+
+instanceForAi.interceptors.request.use((config: InternalAxiosRequestConfig) => {
+  const { method, baseURL, url } = config;
+  logOnDev(`🚀 [API Request] ${method?.toUpperCase()} ${baseURL} ${url}`);
+  const accessToken = useConnectionStore.getState().token;
+
+  config.headers["Authorization"] = `Bearer ${accessToken}`;
+  return config;
+});
+
+export function AudioAiConvert(formData: FormData) {
+  return instanceForAi.post(`/ai/audio`, formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
     },
-    {
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-    },
-  );
+  });
 }
