fbf244d6d7dfa7de631be2000ebc9cfd2af9a1e2
Minhyung Cho <dup33333@hufs.ac.kr>
2024-12-03 16:41:38 +0900

    refactor : 텍스트와 음성 파일 validation 확인 로직 추가

--- client/src/components/MindMapMainSection/ControlSection/VoiceFileUpload.tsx
+++ client/src/components/MindMapMainSection/ControlSection/VoiceFileUpload.tsx
@@ -8,26 +8,37 @@ import { useNodeListContext } from "@/store/NodeListProvider";
 import { useConnectionStore } from "@/store/useConnectionStore";
 import { AudioAiConvert } from "@/api/ai";
 import { useParams } from "react-router-dom";
+import { getMindMapByConnectionId } from "@/api/mindmap.api";
+import { audioFormData } from "@/utils/formData";
+import { FILE_UPLOAD_LIMIT } from "@/constants/uploadLimit";
 
 export default function VoiceFileUpload() {
   const [file, setFile] = useState<File | null>(null);
   const { mindMapId: connectionId } = useParams<{ mindMapId: string }>();
-  const { availabilityInform, handleMouseEnter, handleMouseLeave } = useUpload();
+  const { availabilityInform, handleMouseEnter, handleMouseLeave, errorMsg, updateErrorMsg } = useUpload();
   const { aiCount } = useNodeListContext();
   const handleSocketEvent = useConnectionStore((state) => state.handleSocketEvent);
 
-  async function sendAudioFile() {
+  function fileValidation(file) {
     if (!file) {
-      alert("파일을 선택해주세요.");
-      return;
+      updateErrorMsg("파일을 선택해주세요.");
+      return false;
     }
+    if (file.size < FILE_UPLOAD_LIMIT) {
+      updateErrorMsg("파일 크기는 50MB를 넘을 수 없습니다.");
+      return false;
+    }
+    return true;
+  }
 
+  async function sendAudioFile() {
+    if (!fileValidation(file)) return;
+    updateErrorMsg("");
     handleSocketEvent({ actionType: "audioAiRequest" });
-    const formData = new FormData();
-    formData.append("aiAudio", file);
-    formData.append("mindmapId", "1");
-    formData.append("connectionId", connectionId);
+
     try {
+      const mindMapId = await getMindMapByConnectionId(connectionId);
+      const formData = audioFormData(file, mindMapId, connectionId);
       await AudioAiConvert(formData);
     } catch (error) {
       console.error("업로드 에러:", error);
@@ -35,18 +46,21 @@ export default function VoiceFileUpload() {
   }
 
   return (
-    <div className="flex h-full flex-col gap-6 text-grayscale-100">
+    <div className="flex h-full flex-col text-grayscale-100">
       <UploadBox file={file} setFile={setFile} />
-      <p className="text-grayscale-400">남은 AI 변환 : {aiCount}</p>
-      <Button
-        className="rounded-xl bg-bm-blue p-3 transition hover:brightness-90"
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
-        onClick={sendAudioFile}
-      >
-        만들기
-        <UploadAvailabilityArrowBox content={availabilityInform} />
-      </Button>
+      <p className="mb-5 mt-1 text-grayscale-400">AI 변환 남은 횟수 : {aiCount}번</p>
+      <div className="mb-5 flex w-full flex-col gap-1">
+        <Button
+          className="rounded-xl bg-bm-blue p-3 transition hover:brightness-90"
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+          onClick={sendAudioFile}
+        >
+          만들기
+          <UploadAvailabilityArrowBox content={availabilityInform} />
+        </Button>
+        {errorMsg && <p className="text-red-500">{errorMsg}</p>}
+      </div>
       <div className="flex w-48 justify-end">
         <img src={clovaX} alt="clovaX" />
       </div>
