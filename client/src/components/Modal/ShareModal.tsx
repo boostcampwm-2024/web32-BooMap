10fc56cdc97daa4e08fa5df76eade09c08a8ef21
Minju9187 <alswn1583@naver.com>
2024-12-05 22:38:40 +0900

    fix : input 안 요소 변경 안되도록 수정

--- client/src/components/Modal/ShareModal.tsx
+++ client/src/components/Modal/ShareModal.tsx
@@ -10,7 +10,7 @@ interface ShareModalProps {
 
 export default function ShareModal({ open, closeModal }: ShareModalProps) {
   const [copySuccess, setCopySuccess] = useState(false);
-  const [currentUrl, setCurrentUrl] = useState(window.location.href);
+  const currentUrl = window.location.href;
 
   async function copyLink() {
     try {
@@ -20,11 +20,6 @@ export default function ShareModal({ open, closeModal }: ShareModalProps) {
       console.error("링크 복사 실패!!!");
     }
     setTimeout(() => setCopySuccess(false), 2000);
-  }
-
-  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
-    e.preventDefault();
-    setCurrentUrl(e.target.value);
   }
 
   function handleKeyDown(e: React.KeyboardEvent<HTMLInputElement>) {
@@ -37,8 +32,8 @@ export default function ShareModal({ open, closeModal }: ShareModalProps) {
         <p className="mb-3 text-lg font-bold text-black">협업 링크</p>
         <Input
           type="text"
+          readOnly
           value={currentUrl}
-          onChange={handleChange}
           onClick={(e) => e.currentTarget.select()}
           onKeyDown={handleKeyDown}
           className="h-10 w-full truncate rounded-lg bg-grayscale-200 px-3 py-2 text-grayscale-500 focus:border-transparent focus:outline-none"
